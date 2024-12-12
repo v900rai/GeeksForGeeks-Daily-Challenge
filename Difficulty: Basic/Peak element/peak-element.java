83ccee0a7ad4826ae644babd8ffa19d23da45f26
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-12-12 10:10:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -19,18 +19,19 @@ class GFG {
             }
             Solution obj = new Solution();
             int f = 0;
-            int A = obj.peakElement(a);
+            int idx = obj.peakElement(a);
             int n = a.length;
-            if (A < 0 && A >= n)
+            if (idx < 0 && idx >= n)
                 System.out.println("false");
             else {
-                if (n == 1 && A == 0)
+                if (n == 1 && idx == 0)
                     f = 1;
-                else if (A == 0 && a[0] >= a[1])
+                else if (idx == 0 && a[0] > a[1])
                     f = 1;
-                else if (A == n - 1 && a[n - 1] >= a[n - 2])
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
                     f = 1;
-                else if (A >= 0 && A < n && a[A] >= a[A + 1] && a[A] >= a[A - 1])
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
                     f = 1;
                 else
                     f = 0;
@@ -50,29 +51,14 @@ class GFG {
 class Solution {
 
     public int peakElement(int[] arr) {
-        int n = arr.length;
-        
-        if (n == 1) {
-            return 0; // Only one element, it's the peak
-        }
-        
-        // Check if the first element is a peak
-        if (arr[0] >= arr[1]) {
-            return 0;
-        }
-        
-        // Check if the last element is a peak
-        if (arr[n - 1] >= arr[n - 2]) {
-            return n - 1;
-        }
-        
-        // Check for peak in the middle of the array
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1]) {
-                return i;
-            }
-        }
-        
-        return -1; // No peak found (shouldn't happen with given constraints)
+       int num=arr[0];
+       int ans=-1;
+       for(int i=0; i<arr.length; i++){
+           if(arr[i]>=num){
+               num=arr[i];
+               ans=i;
+           }
+       }
+       return ans;
     }
 }
