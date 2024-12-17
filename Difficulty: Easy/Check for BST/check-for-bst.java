80bdea5de87731b816837bad0974f900959e7e37
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2024-12-17 21:54:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.java
+++ Difficulty: Easy/Check for BST/check-for-bst.java
@@ -118,7 +118,8 @@ class Solution {
     }
     public static boolean bst(Node root,int min,int max){
         if(root == null) return true;
-        if(root.data<=min || root.data>=max) return false;
+        if(root.data<=min || root.data>=max) 
+        return false;
         return bst(root.left,min,root.data) && bst(root.right,root.data,max);
     }
     
