a8104403b66ad81f9e9e221c59035d027fb7050b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-01-19 21:33:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse digits/reverse-digits.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.reverseDigits(n);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int reverseDigits(int n) {
+        int sum = 0;
+        while (n > 0){
+            int digit = n % 10;
+            sum = (sum * 10) + digit;
+            n = n / 10;
+        }
+        return sum;
+    }
+}
