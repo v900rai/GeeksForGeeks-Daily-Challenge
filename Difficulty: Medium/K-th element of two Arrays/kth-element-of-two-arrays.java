b912cc2a1dc741da472c1af4ebe3400ce313123d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-04 11:14:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -37,38 +37,17 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    public int kthElement(int[] a, int[] b, int k) {
-        int n1 = a.length;
-        int n2 = b.length;
-
-        // Ensure that array a is the smaller one
-        if (n1 > n2) {
-            return kthElement(b, a, k);
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        int arr[] = new int[a.length+b.length];
+        System.arraycopy(a,0,arr,0,a.length);
+        System.arraycopy(b,0,arr,a.length,b.length);
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+        if(i==k-1){
+            return arr[i];
         }
-
-        int low = Math.max(0, k - n2);
-        int high = Math.min(k, n1);
-
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = k - cut1;
-
-            int left1 = (cut1 > 0) ? a[cut1 - 1] : Integer.MIN_VALUE;
-            int left2 = (cut2 > 0) ? b[cut2 - 1] : Integer.MIN_VALUE;
-
-            int right1 = (cut1 < n1) ? a[cut1] : Integer.MAX_VALUE;
-            int right2 = (cut2 < n2) ? b[cut2] : Integer.MAX_VALUE;
-
-            // Check if the partition is correct
-            if (left1 <= right2 && left2 <= right1) {
-                return Math.max(left1, left2);
-            } else if (left1 > right2) {
-                high = cut1 - 1;
-            } else {
-                low = cut1 + 1;
-            }
         }
-
-        return -1; // Should not reach here
+        return 0;
     }
 }
