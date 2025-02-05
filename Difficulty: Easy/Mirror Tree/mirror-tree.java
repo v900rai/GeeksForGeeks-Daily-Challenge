ddf725baf7ab84464aed4fe7093519b57c8fb540
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-05 11:55:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Mirror Tree/mirror-tree.java
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -2,15 +2,16 @@
 // Initial Template for Java
 
 // Contributed by Sudarshan Sharma
-import java.util.LinkedList;
-import java.util.Queue;
 import java.io.*;
 import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Node {
     int data;
     Node left;
     Node right;
+
     Node(int data) {
         this.data = data;
         left = null;
@@ -75,20 +76,36 @@ class GfG {
 
         return root;
     }
-    void inOrder(Node node) {
-        if (node == null) {
-            return;
+
+    static String levelOrder(Node root) {
+        if (root == null) return "N\n";
+
+        StringBuilder str = new StringBuilder();
+        Queue<Node> qq = new LinkedList<>();
+        qq.add(root);
+
+        while (!qq.isEmpty()) {
+            Node curr = qq.poll();
+
+            if (curr == null) {
+                str.append("N ");
+                continue;
+            }
+            str.append(curr.data).append(" ");
+            qq.add(curr.left);
+            qq.add(curr.right);
         }
 
-        inOrder(node.left);
-        System.out.print(node.data + " ");
+        // Trim trailing non-digit characters
+        while (str.length() > 0 && !Character.isDigit(str.charAt(str.length() - 1))) {
+            str.deleteCharAt(str.length() - 1);
+        }
 
-        inOrder(node.right);
+        return str.toString();
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int t = Integer.parseInt(br.readLine());
 
@@ -98,14 +115,15 @@ class GfG {
 
             Solution g = new Solution();
             g.mirror(root);
-            GfG mt = new GfG();
-            mt.inOrder(root);
-            System.out.println();
+            System.out.println(levelOrder(root));
+            System.out.println("~");
         }
     }
 }
 
 // } Driver Code Ends
+
+
 
 
 // User function Template for Java
