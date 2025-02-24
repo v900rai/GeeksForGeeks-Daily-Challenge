bf92fc322598060f3bd5c0853c99b3e4a73fd742
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-24 11:32:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -35,16 +35,22 @@ class Solution {
         
         for(int i=0; i<s.length(); i++){
             char c = s.charAt(i);
-            if(c=='(' || c=='{' || c=='[') stack.push(c);
+            if(c=='(' || c=='{' || c=='[') 
+            stack.push(c);
             else {
-                if(stack.size() ==0) return false;
-                else if(c == '}' && stack.peek() != '{')  return false;
-                else if(c == ']' && stack.peek() != '[')  return false;
-                if(c == ')' && stack.peek() != '(')  return false;
+                if(stack.size() ==0)
+                return false;
+                else if(c == '}' && stack.peek() != '{')  
+                return false;
+                else if(c == ']' && stack.peek() != '[') 
+                return false;
+                if(c == ')' && stack.peek() != '(') 
+                return false;
                 else stack.pop();
             }
         }
-        if(stack.size() > 0) return false;
+        if(stack.size() > 0) 
+        return false;
         return true;
     }
 }
