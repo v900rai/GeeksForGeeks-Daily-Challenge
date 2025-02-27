add5f2dd235909097ffebfa86d7c7d68dd92ae24
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-02-27 11:50:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
+++ Difficulty: Medium/Detect Loop in linked list/detect-loop-in-linked-list.java
@@ -89,7 +89,8 @@ class Solution {
         while(fast!=null&&fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-            if(slow==fast) return true;
+            if(slow==fast)
+            return true;
         }
         return false;
      
