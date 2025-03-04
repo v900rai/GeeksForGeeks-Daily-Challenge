af2ceec03ed708dff946620fdf6d0c52fe184995
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-04 11:36:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Geeks {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine())
+                               .trim()
+                               .split(" "); // Read the input for the current test case
+            int arr[] = new int[str.length];
+            // Convert input string into an integer array
+            for (int i = 0; i < str.length; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            // Call the solution method and print the result
+            System.out.println(new Solution().lis(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static int lis(int arr[]) {
+        int n = arr.length;
+        List<Integer> ans = new ArrayList<>();
+        ans.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > ans.get(ans.size() - 1)) {
+                ans.add(arr[i]);
+            } else {
+                int low = Collections.binarySearch(ans, arr[i]);
+                if (low < 0) {
+                    low = -(low + 1);
+                }
+                ans.set(low, arr[i]);
+            }
+        }
+        return ans.size();
+    }
+}
