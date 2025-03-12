f89757f448874f1ee66f62bf3695f816a3458d74
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-12 12:25:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+// } Driver Code Ends
+
+//User function Template for Java
+
+public static int nSum(int n){
+   int ans = 0;    
+    for(int i=1;i<=n;i++){
+        ans=ans+i;
+    }
+    return ans;
+   
+}
+
+
+//{ Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int n = scn.nextInt();
+            int ans = nSum(n);
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+        scn.close();
+    }
+}
+// } Driver Code Ends
