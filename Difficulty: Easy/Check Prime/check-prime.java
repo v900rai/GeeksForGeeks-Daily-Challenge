d9c7f33e731304259cdb7925658f2ea4e9d510ab
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-12 12:38:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.java
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -1,50 +1,41 @@
 //{ Driver Code Starts
-//Initial Template for Java
 
-import java.util.Scanner;
-
-class Main {
-
-    
 // } Driver Code Ends
 
-//User function Template for Java
+import java.util.*;
 
-public static boolean prime(int n){
+
+public class Main {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        int N= sc.nextInt();
+        
+        if(isPrime(N) == 1)
+        
+            System.out.print("True");
+            
+        else
+        
+         System.out.print("False");
+        
+    }
     
-    int count=0;
-    for(int i=1; i<=n; i++){
-        if(n%i==0){
-            count++;
+        public static int isPrime(int n){
+            
+            if(n<=1)
+            return 0;
+            
+            for(int i=2;i<=Math.sqrt(n);i++){
+                if(n%i==0)
+                    return 0;
+            }
+            return 1;
         }
-    }
-    if(count==2){
-        return true;
-    }
-    else{
-        return false;
-    }
-    
 }
+    
+
 
 
 //{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n = scn.nextInt();
-            boolean ans = prime(n);
-            if(ans)
-            System.out.println("True");
-            else
-            System.out.println("False");
-        
-System.out.println("~");
-}
-        scn.close();
-    }
-}
 // } Driver Code Ends
