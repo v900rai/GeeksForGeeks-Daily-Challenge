2d8ed4c57dbbb540ba5dc952eb643ec3cc32e64e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-22 20:33:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Mirror Tree/mirror-tree.java
+++ Difficulty: Easy/Mirror Tree/mirror-tree.java
@@ -124,13 +124,8 @@ class GfG {
 // } Driver Code Ends
 
 
-
-
 // User function Template for Java
 
-// function Template for Java
-
-// FUNCTION CODE
 /* A Binary Tree node
 class Node
 {
@@ -146,17 +141,13 @@ class Node
 class Solution {
     // Function to convert a binary tree into its mirror tree.
     void mirror(Node node) {
-        // Your code here
-        if( node == null){
-            return;
-        }
-
-//use temp and just swap it
-        Node temp = node.left;
-        node.left = node.right;
-        node.right = temp;
-        mirror(node.right);
-        mirror(node.left);
-         
+       if(node ==null){
+           return ;
+       }
+       Node temp=node.left;
+       node.left=node.right;
+       node.right=temp;
+       mirror(node.left);
+       mirror(node.right);
     }
 }
