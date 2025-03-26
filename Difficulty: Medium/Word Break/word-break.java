b34ca0b1c404e9628bf34a30b08ef104f39efe44
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-03-26 11:12:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Word Break/word-break.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GfG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine();
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String line = sc.nextLine();
+            String[] dictionary = line.split(" ");
+
+            Solution obj = new Solution();
+            if (obj.wordBreak(s, dictionary)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public boolean wordBreak(String s, String[] wordDict) {
+        int n=s.length();
+        HashSet<String> set = new HashSet<>(Arrays.asList(wordDict));
+        boolean[] dp=new boolean[n+1];
+        dp[0]=true;
+        int maxWordLength = 0;
+        for (String word : wordDict) {
+            maxWordLength = Math.max(maxWordLength, word.length());
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=Math.max(i-maxWordLength,0);j<i;j++){
+                if(dp[j]&& set.contains(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
+
