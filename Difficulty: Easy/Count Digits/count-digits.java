8e8a02a6b2efb76feee7a8fc7c3570cc36a85506
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-05 19:24:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Count Digits/count-digits.java
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -1,40 +1,42 @@
 //{ Driver Code Starts
 // Initial Template for Java
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-class Main {
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-
-public static int countDigits(int n) {
-
-    // write your code here
-    // return number of digits in n
-   int count=0;
-   while(n>0){
-       n=n/10;
-       count++;
-   }
-   return count;
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
         while (t-- > 0) {
-            int n = scn.nextInt();
-            System.out.println(countDigits(n));
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
 
             System.out.println("~");
         }
-        scn.close();
     }
 }
 // } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int evenlyDivides(int n) {
+        // code here
+        int m=n;
+        int count=0;
+        while(m>0){
+            int digit=m%10;
+            if(digit>0){
+                if(n%digit==0){
+                    count+=1;
+                }
+            }
+            m/=10;
+        }
+        
+        return count;
+        
+    }
+}
