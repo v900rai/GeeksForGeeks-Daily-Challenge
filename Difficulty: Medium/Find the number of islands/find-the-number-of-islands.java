013adc6bdade320c04143507cb342bb8710edd88
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-05 17:22:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,65 +1,64 @@
 //{ Driver Code Starts
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tc = scanner.nextInt();
+        while (tc-- > 0) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
             char[][] grid = new char[n][m];
+
+            // Read the grid input
             for (int i = 0; i < n; i++) {
-                String[] S = br.readLine().trim().split(" ");
                 for (int j = 0; j < m; j++) {
-                    grid[i][j] = S[j].charAt(0);
+                    grid[i][j] = scanner.next().charAt(0);
                 }
             }
             Solution obj = new Solution();
-            int ans = obj.numIslands(grid);
+            int ans = obj.countIslands(grid);
             System.out.println(ans);
+            System.out.println("~");
         }
+        scanner.close();
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    public void dfsIterative(int i,int j,char grid[][],boolean vis[][],int dir[][]){
-        Stack<int[]> st=new Stack<>();
-        st.push(new int[]{i,j});
-        vis[i][j]=true;
-        grid[i][j]='0';
-        while(!st.isEmpty()){
-            int[] curr=st.pop();
-            int curri=curr[0],currj=curr[1];
-            for(int[] d:dir){
-                int ni=curri+d[0],nj=currj+d[1];
-                if(ni>=0 && ni<grid.length && nj>=0 && nj<grid[0].length && grid[ni][nj]=='1' && !vis[ni][nj]){
-                    st.push(new int[]{ni,nj});
-                    vis[ni][nj]=true;
-                    grid[ni][nj]='0';
-                }
-            }
-        }
-    }
-    // Function to find the number of islands.
-    public int numIslands(char[][] grid) {
+       public int countIslands(char[][] grid) {
         // Code here
-        int n=grid.length,m=grid[0].length,islands=0;
-        boolean vis[][]=new boolean[n][m];
-        int dir[][]=new int[][]{{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    dfsIterative(i,j,grid,vis,dir);
-                    islands++;
+                if(grid[i][j]=='L'){
+                    dfs(grid,i,j,n,m);
+                    count++;
                 }
             }
         }
-        return islands;
+        return count;
+        
     }
+    public static void dfs(char[][] grid,int i,int j,int n,int m){
+         
+        grid[i][j]='K';
+        for(int r=-1;r<=1;r++){
+            for(int c=-1;c<=1;c++){
+                int nr=i+r;
+                int nc=j+c;
+                if(nr>=0 && nr<n && nc<m && nc>=0 && grid[nr][nc]=='L'){
+                   dfs(grid,nr,nc,n,m);
+                }
+            }
+        }
+        
+    }
+
+
 }
