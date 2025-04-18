4cb8c1b175775ec1a621c8376262832e89f28ac0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-04-18 09:48:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -14,42 +14,32 @@ class Geeks {
             int arr[] = new int[str.length];
             for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
             System.out.println(new Solution().majorityElement(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-
-
 // User function Template for Java
 
+
 class Solution {
-    
     static int majorityElement(int arr[]) {
-        int n = arr.length;
-
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-
-            // Count occurrences of arr[i]
-            for (int j = 0; j < n; j++) {
-                if (arr[i] == arr[j]) {
-                    count++;
-                }
-            }
-
-            // Check if count is greater than n/2
-            if (count > n / 2) {
+        if(arr.length==1)
+        return arr[0];
+        int size=arr.length/2;
+        int count=1;
+        Arrays.sort(arr);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==arr[i-1]){
+            count++;
+                if(count>size)
                 return arr[i];
             }
+            else
+            count=1;
         }
-
-        // If no majority element exists
         return -1;
-    
-
-   
-
     }
 }
