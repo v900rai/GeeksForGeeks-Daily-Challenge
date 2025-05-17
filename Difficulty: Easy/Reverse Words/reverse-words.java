8a2c7263c2164c7b845cd89379a8f0f8db73cab8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-17 12:06:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -1,45 +1,57 @@
 //{ Driver Code Starts
+// Initial Template for Java
+
 import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.util.Scanner;
 
-class GFG {
+class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            String s = sc.next();
-            Solution obj = new Solution();
-            System.out.println(obj.reverseWords(s));
-            t--;
+
+        int t = sc.nextInt(); // Read the number of test cases
+        sc.nextLine();        // Consume the newline character after reading the integer
+
+        while (t-- > 0) {
+            String s = sc.nextLine(); // Read the input string
+
+            // Remove surrounding quotes from the string
+            String str = s.substring(1, s.length() - 1);
+
+            Solution obj = new Solution(); // Create an object of the Solution class
+            String ans = obj.reverseWords(str); // Reverse the words in the string
+
+            // Print the result with surrounding quotes
+            System.out.println("\"" + ans + "\"");
         }
+        sc.close();
     }
 }
 
 // } Driver Code Ends
 
-
-
-
-
-class Solution 
-{
-    //Function to reverse words in a given string.
-    String reverseWords(String S)
-    
-    {
-      
-        // code here 
-        String[] words = S.split("\\.");
+class Solution {
+    // Function to reverse words in a given string
+    public String reverseWords(String s) {
+        // Trim the string to remove leading and trailing spaces
+        s = s.trim();
         
-        String rev ="";
-        for(int i=words.length-1; i>=0; i--){
-            rev+=words[i];
-            if(i>0){
-                rev+=".";
+        // Split the string by spaces using regex to handle multiple spaces
+        String[] words = s.split("\\s+");
+        
+        // Use StringBuilder for efficient string manipulation
+        StringBuilder reversed = new StringBuilder();
+        
+        // Traverse the words array in reverse order
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i != 0) {
+                reversed.append(" ");
+            }
+        }
+        
+        // Convert StringBuilder to String and return
+        return reversed.toString();
     }
 }
-return rev;
-}
-}
+
