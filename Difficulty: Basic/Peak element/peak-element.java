6fe293f647c2b2ba39aaec373ff3f051b5e74e91
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-23 13:23:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Peak element/peak-element.java
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -1,67 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int a[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                a[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int f = 0;
-            int idx = obj.peakElement(a);
-            int n = a.length;
-            if (idx < 0 && idx >= n)
-                System.out.println("false");
-            else {
-                if (n == 1 && idx == 0)
-                    f = 1;
-                else if (idx == 0 && a[0] > a[1])
-                    f = 1;
-                else if (idx == n - 1 && a[n - 1] > a[n - 2])
-                    f = 1;
-                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
-                         a[idx] > a[idx - 1])
-                    f = 1;
-                else
-                    f = 0;
-                if (f == 1) {
-                    System.out.println("true");
-                } else {
-                    System.out.println("false");
-                }
-            }
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-
-
 class Solution {
 
     public int peakElement(int[] arr) {
-        int max = 0;
-        int ans= 0;
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-                ans = i;
+      
+        int n=arr.length;
+        int max=0;
+        int index=0;
+        for(int i=0; i<n; i++){
+            if(max<arr[i]){
+                max=arr[i];
+                index=i;
             }
         }
-        return ans;
+        return index;
     }
 }
