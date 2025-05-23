99122ff38bae9294807a1aad73c8da62ae869aed
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-23 13:29:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.java
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
-
-        // Loop for each test case
-        while (t-- > 0) {
-            String line = read.readLine().trim(); // Read the array input
-            String[] numsStr = line.split(" ");   // Split the input string by spaces
-            int[] nums =
-                new int[numsStr.length]; // Convert string array to integer array
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
-            }
-
-            int target = Integer.parseInt(read.readLine().trim()); // Read target sum
-
-            Solution ob = new Solution(); // Create an object of Solution class
-            System.out.println(
-                ob.perfectSum(nums, target)); // Call perfectSum and print the result
-            System.out.println("~");          // Call perfectSum and print the result
-        }
-    }
-}
-// } Driver Code Ends
 
 class Solution {
     public int perfectSum(int[] nums, int target) {
