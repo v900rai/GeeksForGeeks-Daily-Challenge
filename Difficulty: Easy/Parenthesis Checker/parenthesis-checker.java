54fd79b9b11f062313bf246b7c3d283635ba9e18
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-26 11:38:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -30,34 +30,47 @@ class Driverclass {
 
 
 
+import java.util.Stack;
 
 class Solution {
+    // Method to check if the given string has balanced parentheses, braces, and brackets
     static boolean isBalanced(String s) {
+        // Create a stack to keep track of opening brackets
         Stack<Character> stack = new Stack<>();
         
-        for(int i=0; i<s.length(); i++){
+        // Iterate through each character in the string
+        for(int i=0; i<s.length(); i++) {
             char c = s.charAt(i);
-            if(c=='(' || c=='{' || c=='[') 
-            stack.push(c);
+            
+            // If it's an opening bracket, push to stack
+            if(c=='(' || c=='{' || c=='[') {
+                stack.push(c);
+            }
             else {
-                if(stack.size() ==0)
-                return false;
+                // If stack is empty when encountering a closing bracket, it's unbalanced
+                if(stack.size() == 0)
+                    return false;
                 
-                
+                // Check if the closing bracket matches the most recent opening bracket
                 else if(c == '}' && stack.peek() != '{')  
-                return false;
+                    return false;
                 
                 else if(c == ']' && stack.peek() != '[') 
-                return false;
+                    return false;
                 
-                if(c == ')' && stack.peek() != '(') 
-                return false;
+                else if(c == ')' && stack.peek() != '(') 
+                    return false;
                 
-                else stack.pop();
+                // If it matches, pop the opening bracket from stack
+                else 
+                    stack.pop();
             }
         }
+        
+        // If stack is not empty at the end, some brackets weren't closed
         if(stack.size() > 0) 
-        return false;
+            return false;
+            
         return true;
     }
 }
