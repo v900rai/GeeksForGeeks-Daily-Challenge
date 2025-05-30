fad794276034da1d940283405d5220d656a1f7f0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-30 11:39:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 
 // User function Template for Java
 // User function Template for Java
@@ -52,51 +43,3 @@ class Solution {
 }
 
 
-
-//{ Driver Code Starts.
-
-class Pair<K, V> {
-    private final K key;
-    private final V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() { return key; }
-
-    public V getValue() { return value; }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            Solution ob = new Solution();
-            Pair<Integer, Integer> pp = ob.getMinMax(arr);
-            System.out.println(pp.getKey() + " " + pp.getValue());
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
