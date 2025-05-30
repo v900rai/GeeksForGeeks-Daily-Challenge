52f18b1f1d7a38dd4928a372031fa2f121a312ca
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-30 11:41:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.java
@@ -1,55 +1,23 @@
-//{ Driver Code Starts
-// // Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-            Solution ob = new Solution();
-            ob.rotate(arr);
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 // // User function Template for Java
 
 class Solution {
     public void rotate(int[] arr) {
-        
+        /*
         int n = arr.length;
         for (int i=0; i<n; i++) {
             int temp = arr[i];
             arr[i] = arr[n-1];
             arr[n-1] = temp;
+        }*/
+        int i = 0, j = arr.length - 1;
+        
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
         }
     }
 }
