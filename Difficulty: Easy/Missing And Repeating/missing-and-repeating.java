df450496d216563cdeced26bdd48dc4283fb4218
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-30 12:49:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList; // Import ArrayList
-import java.util.Stack;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputs = input.split(" ");
-            int[] arr = new int[inputs.length];
-
-            for (int i = 0; i < inputs.length; i++) {
-                arr[i] = Integer.parseInt(inputs[i]);
-            }
-
-            // Get the result as an ArrayList<Integer>
-            ArrayList<Integer> ans = new Solution().findTwoElement(arr);
-            System.out.println(ans.get(0) + " " + ans.get(1));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
