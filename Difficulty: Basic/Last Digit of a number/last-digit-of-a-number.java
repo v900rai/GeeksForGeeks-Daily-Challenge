d25a1c343b5ba1d15c1094e586d5dd67e2066569
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-05-31 20:31:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.java
+++ Difficulty: Basic/Last Digit of a number/last-digit-of-a-number.java
@@ -1,38 +1,10 @@
-//{ Driver Code Starts
-//Initial Template for Java
+class Solution {
+    public static void utility(int n) {
 
-import java.util.Scanner;
+        // just complete below statement
+        int ans =Math.abs(n%10);
 
-class Main {
-    
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static void utility(int n){
-
-    //just complete below statement
-   int y = Math.abs(n);
-    //just complete below statement
-    int ans = y%10;
-
-    //below statement print the result
-    System.out.println(ans);
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n = scn.nextInt();
-            utility(n);
-        }
-        scn.close();
+            // below statement print the result
+            System.out.println(ans);
     }
 }
-// } Driver Code Ends
