dd9140c5ed696b017b24b508284b07184b26db9d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-14 18:48:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Symmetric Tree/symmetric-tree.java
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.java
@@ -23,8 +23,10 @@ class Solution {
         
     }
     public static boolean check(Node head1,Node head2){
-        if(head1==null && head2==null)return true;
-        if(head1==null || head2==null || head1.data!=head2.data)return false;
+        if(head1==null && head2==null)
+        return true;
+        if(head1==null || head2==null || head1.data!=head2.data)
+        return false;
         
      boolean left=check(head1.left,head2.right);
      boolean  right=check(head1.right,head2.left);
