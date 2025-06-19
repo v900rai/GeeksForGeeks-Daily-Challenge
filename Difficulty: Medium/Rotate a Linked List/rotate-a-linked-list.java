ebcc13cc476d9afce4403f722d1e12993dff26f0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-19 11:41:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.java
@@ -1,21 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-
-// } Driver Code Ends
 /* node of linked list:
 
 class Node{
@@ -61,51 +43,3 @@ class Solution {
     }
 }
 
-
-//{ Driver Code Starts.
-
-public class GFG {
-    static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-
-        while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = br.readLine().trim();
-            if (!input.isEmpty()) {
-                String[] numbers = input.split("\\s+");
-                for (String num : numbers) {
-                    if (!num.isEmpty()) {
-                        arr.add(Integer.parseInt(num));
-                    }
-                }
-            }
-
-            Node head = null;
-            if (!arr.isEmpty()) {
-                head = new Node(arr.get(0));
-                Node tail = head;
-                for (int i = 1; i < arr.size(); ++i) {
-                    tail.next = new Node(arr.get(i));
-                    tail = tail.next;
-                }
-            }
-            int k = Integer.parseInt(br.readLine().trim());
-            Solution ob = new Solution();
-            head = ob.rotate(head, k);
-            printList(head);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
