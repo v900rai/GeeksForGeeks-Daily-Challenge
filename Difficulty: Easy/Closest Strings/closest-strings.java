1d8ba6ea3dbd5859b6eb7a44509e260b8e76a360
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-20 12:03:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Closest Strings/closest-strings.java
@@ -0,0 +1,29 @@
+// User function Template for Java
+
+class Solution {
+    int shortestDistance(ArrayList<String> s, String word1, String word2) {
+        
+        int shortest = Integer.MAX_VALUE;
+        int index1 = -1;
+        int index2 = -1;
+        
+        for(int i=0;i<s.size();i++){
+            String str = s.get(i);
+            
+            if(str.equals(word1)){
+                index1 = i;
+            }
+            
+            if(str.equals(word2)){
+                index2  = i;
+            }
+            
+            if(index1 != -1 && index2 != -1){
+                int distance = Math.abs(index1 - index2);
+                shortest = distance < shortest ? distance : shortest;
+            }
+        }
+        return shortest;
+    }
+}
+
