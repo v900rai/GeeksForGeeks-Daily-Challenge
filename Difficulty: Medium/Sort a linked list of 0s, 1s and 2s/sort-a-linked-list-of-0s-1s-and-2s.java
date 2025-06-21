25f405dba276f6663f7f7c6c37b2b9c08d6ccf65
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-21 12:04:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -1,19 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-
-// } Driver Code Ends
 
 /* class Node
 {
@@ -28,7 +12,9 @@ class Node {
 class Solution {
    static Node segregate(Node head) {
         // code here
-        int zero = 0, one = 0, two = 0;
+        int zero = 0;
+        int one = 0;
+        int two = 0;
         Node curr = head;
         while(curr!=null){
             if(curr.data==0){
@@ -66,43 +52,3 @@ class Solution {
 }
 
 
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void printList(Node node) {
-        while (node != null) {
-            System.out.print(node.data + " ");
-            node = node.next;
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-
-        while (t-- > 0) {
-            List<Integer> arr = new ArrayList<>();
-            String input = br.readLine().trim();
-            StringTokenizer st = new StringTokenizer(input);
-            while (st.hasMoreTokens()) {
-                arr.add(Integer.parseInt(st.nextToken()));
-            }
-
-            Node head = null;
-            if (!arr.isEmpty()) {
-                head = new Node(arr.get(0));
-                Node tail = head;
-                for (int i = 1; i < arr.size(); i++) {
-                    tail.next = new Node(arr.get(i));
-                    tail = tail.next;
-                }
-            }
-            head = new Solution().segregate(head);
-            printList(head);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
