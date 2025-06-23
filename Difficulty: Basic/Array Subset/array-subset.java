5994c151be7ce07e789c66bca73d6f8dfb6f2ec9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-23 11:27:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.java
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -1,60 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
-        while (t-- > 0) {
-            String line = read.readLine().trim();
-            String[] numsStr = line.split(" ");
-            int[] a = new int[numsStr.length];
-            for (int i = 0; i < numsStr.length; i++) {
-                a[i] = Integer.parseInt(numsStr[i]);
-            }
-            line = read.readLine().trim();
-            numsStr = line.split(" ");
-            int[] b = new int[numsStr.length];
-            for (int i = 0; i < numsStr.length; i++) {
-                b[i] = Integer.parseInt(numsStr[i]);
-            }
-            Solution ob = new Solution();
-            if (ob.isSubset(a, b))
-                System.out.println("true");
-            else
-                System.out.println("false");
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
 
 
 class Solution {
     public boolean isSubset(int a[], int b[]) {
+        // Your code here
         Arrays.sort(a);
-        Arrays.sort(b); 
-        
-        HashSet<Integer> set = new HashSet<>();
-        for(int i: a){
-            set.add(i); 
-        }
-        
-        for(int i: b){
-            if(!set.contains(i)){
-                return false;  
+        Arrays.sort(b);
+        int count = 0;
+        int i = 0, j =0;
+        for(;i<a.length && j < b.length;i++){
+            if(a[i] == b[j]){
+                count++;
+                j++;
             }
         }
-        return true; 
+        return count==b.length;
     }
 }
