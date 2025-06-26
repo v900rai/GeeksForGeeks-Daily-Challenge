c7bc609eced7015152d7e4d107d678d786366d81
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-26 20:18:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Reverse alternate nodes in Link List/reverse-alternate-nodes-in-link-list.java
@@ -0,0 +1,24 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class Solution {
+    public static void rearrange(Node odd) {
+        Node extracted = null;
+        while (odd.next != null) {
+            Node newNext = odd.next.next;
+            odd.next.next = extracted;
+            extracted = odd.next;
+            odd.next = newNext;
+            if (newNext != null) odd = newNext;
+        }
+        odd.next = extracted;
+}
+}
