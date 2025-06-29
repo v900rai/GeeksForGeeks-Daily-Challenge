15ef6275780738f49d936b22b659afb044532ac5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-06-29 18:08:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.java
+++ Difficulty: Hard/Stock Buy and Sell – Max 2 Transactions Allowed/stock-buy-and-sell-max-2-transactions-allowed.java
@@ -1,28 +1,12 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().maxProfit(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 class Solution {
     public static int maxProfit(int[] prices) {
         // code here
-        int buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
-        int sell1 = 0, sell2 = 0;
+        int buy1 = Integer.MIN_VALUE;
+        int buy2 = Integer.MIN_VALUE;
+        int sell1 = 0;
+        int sell2 = 0;
        
         for (int price : prices) {
             buy1 = Math.max(buy1, -price);
