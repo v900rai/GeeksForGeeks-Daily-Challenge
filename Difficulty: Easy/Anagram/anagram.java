69abc611aca8935a4179cb4afddba364df9f9c95
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-03 12:14:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -3,6 +3,9 @@
 class Solution {
     // Function is to check whether two strings are anagram of each other or not.
     public static boolean areAnagrams(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
 
        char onechar [] =s1.toCharArray();
        char twochar [] =s2.toCharArray();
