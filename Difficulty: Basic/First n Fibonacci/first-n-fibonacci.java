782b47d93c109b75facdfb93799a64d63583dc2a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-03 12:57:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -1,52 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-
-        // taking input using Scanner class
-        Scanner sc = new Scanner(System.in);
-
-        // taking total count of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // taking total number of elements
-            int n = sc.nextInt();
-
-            // calling fibonacciNumbers() method
-            int[] res = new Solution().fibonacciNumbers(n);
-
-            // printing the elements of the array
-            for (int i = 0; i < res.length; i++) System.out.print(res[i] + " ");
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     // Function to return list containing first n fibonacci numbers.
     public static int[] fibonacciNumbers(int n) {
-      if (n <= 0) return new int[]{};
-        if (n == 1) return new int[]{0};
-        int[] arr = new int[n];
-        arr[0] = 0;
-        arr[1] = 1;
-        for (int i = 2; i < n; i++) {
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-        
-        return arr;
+        // Your code here
+           // Your code here
+        int[] ans=new int[n];
+        ans[0]=0;
+        if(n==1) return ans;
+        ans[1]=1;
+        if(n==2) return ans;
+        for(int i=2;i<n;i++) ans[i]=ans[i-1]+ans[i-2];
+        return ans;
     }
 }
