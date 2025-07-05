beba5f4aae35c868f751cd4d4460923bacb9d5df
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-05 18:10:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int r = sc.nextInt();
-            int c = sc.nextInt();
-
-            int matrix[][] = new int[r][c];
-
-            for (int i = 0; i < r; i++) {
-                for (int j = 0; j < c; j++) matrix[i][j] = sc.nextInt();
-            }
-            Solution ob = new Solution();
-            ArrayList<Integer> ans = ob.spirallyTraverse(matrix);
-            for (Integer val : ans) System.out.print(val + " ");
-            System.out.println();
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 
