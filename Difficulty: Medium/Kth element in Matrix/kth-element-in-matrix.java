192ccba3948071ee417620dd6a3d51c37ac41646
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-05 18:25:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
+++ Difficulty: Medium/Kth element in Matrix/kth-element-in-matrix.java
@@ -1,17 +1,38 @@
 class Solution {
-    public int kthSmallest(int[][] matrix, int k) {
-        // code here
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        int n = matrix.length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                q.add(matrix[i][j]);
-            }
+   
+
+public int kthSmallest(int[][] matrix, int k) {
+    int n = matrix.length;
+    int low = matrix[0][0];
+    int high = matrix[n-1][n-1];
+    
+    while (low < high) {
+        int mid = low + (high - low) / 2;
+        int count = countLessEqual(matrix, mid);
+        
+        if (count < k) {
+            low = mid + 1;
+        } else {
+            high = mid;
         }
-        int res = 0;
-        for(int i=0;i<k;i++){
-            res = q.remove();
-        }
-        return res;
     }
+    return low;
+}
+
+private int countLessEqual(int[][] matrix, int target) {
+    int count = 0;
+    int n = matrix.length;
+    int i = n - 1;
+    int j = 0;
+    
+    while (i >= 0 && j < n) {
+        if (matrix[i][j] > target) {
+            i--;
+        } else {
+            count += i + 1;
+            j++;
+        }
+    }
+    return count;
+}
 }
