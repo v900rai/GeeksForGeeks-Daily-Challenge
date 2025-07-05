3a2ffc4dd2bcd680eff6bdbf07720ae6059b57fb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-05 18:04:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.java
+++ Difficulty: Medium/Max Score from Subarray Mins/max-score-from-subarray-mins.java
@@ -1,10 +1,23 @@
 
+// class Solution {
+//     public int maxSum(int arr[]) {
+//         int ans=Integer.MIN_VALUE;
+//         for(int i=1;i<arr.length;i++){
+//             ans=Math.max(ans,arr[i]+arr[i-1]);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int maxSum(int arr[]) {
-        int ans=Integer.MIN_VALUE;
-        for(int i=1;i<arr.length;i++){
-            ans=Math.max(ans,arr[i]+arr[i-1]);
+        // code here
+        int n=arr.length;
+        int maxSum=0;
+        for(int i=0;i<n-1;i++)
+        {
+            int sum=arr[i]+arr[i+1];
+            maxSum=Math.max(maxSum,sum);
         }
-        return ans;
+        return maxSum;
     }
 }
