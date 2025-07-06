358bbdcfccd88f54a12c85abe313ff7a694f14c5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-06 20:38:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/nCr/n-cr.java
+++ Difficulty: Medium/nCr/n-cr.java
@@ -1,25 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            int n = sc.nextInt();
-            int r = sc.nextInt();
-            Solution ob = new Solution();
-            System.out.println(ob.nCr(n, r));
-            System.out.println("~");
-        }
-
-        sc.close();
-    }
-}
-// } Driver Code Ends
 
 class Solution {
     public int nCr(int n, int r) {
