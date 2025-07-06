e66b1645c6b30205395e25199f0363a75b8f14ee
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-06 20:58:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -1,27 +1,20 @@
-
-
-// User function Template for Java
-
-
-
 class Solution {
-    public static int findUnion(int a[], int b[]) {
-        HashSet<Integer> unionSet = new HashSet<>();
-        for (int i=0; i<a.length; i++) {
-            unionSet.add(a[i]);
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        // Using HashSet to remove duplicates
+        HashSet<Integer> set = new HashSet<>();
+        
+        for (int num : a) {
+            set.add(num);
+        }
+        for (int num : b) {
+            set.add(num);
         }
         
-        for(int j=0; j<b.length; j++){
-            unionSet.add(b[j]);
-        }
+       ArrayList<Integer> result = new ArrayList<>(set);
         
-        return unionSet.size();
+     
+       Collections.sort(result);
+        
+        return result;
     }
-
-    /*public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4};
-        int[] b = {3, 4, 5, 6};
-        
-        System.out.println("Size of Union: " + findUnion(a, b)); // Output: 6
-    }*/
 }
