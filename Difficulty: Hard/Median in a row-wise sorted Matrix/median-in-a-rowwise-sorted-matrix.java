4c6ae16be63cfb7e43ef7281c59b8127df934580
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-07 15:14:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+   int median(int mat[][]) {
+        // code here
+        // traversing matrix with O(n*m)
+        int row = mat.length;
+        int col = mat[0].length;
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0,r=0,c=0;i<row*col;i++)
+        {
+            list.add(mat[r][c]);
+            c++;
+            if(c == col)
+            {
+                c=0;
+                r++;
+            }
+        }
+        Collections.sort(list);
+        
+        return list.get(list.size()/2);
+    }
+}
