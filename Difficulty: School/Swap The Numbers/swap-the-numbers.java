b67812f91e60ffcfd741a76b45d316079b028ac6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-09 21:20:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: School/Swap The Numbers/swap-the-numbers.java
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.java
@@ -1,33 +1,17 @@
-//{ Driver Code Starts
-// Initial Template for Java
+// User function Template for Java
 
+// User function Template for Java
 import java.io.*;
 import java.util.*;
-
 class GFG {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            Solution oj = new Solution();
-            oj.swap(a, b);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
-class Solution {
-    public void swap(int a, int b) {
-        int temp=a;
-        a=b;b=temp;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Code to Swap
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
 
         System.out.println(a + " " + b);
     }
