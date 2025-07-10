7fa2b931429263e99175dd9c029cfa0f1a946aa6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-10 15:57:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.java
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt(); // Read the number of test cases
-        sc.nextLine();        // Consume the newline character after reading the integer
-
-        while (t-- > 0) {
-            String s = sc.nextLine(); // Read the input string
-
-            // Remove surrounding quotes from the string
-            String str = s.substring(1, s.length() - 1);
-
-            Solution obj = new Solution(); // Create an object of the Solution class
-            String ans = obj.reverseWords(str); // Reverse the words in the string
-
-            // Print the result with surrounding quotes
-            System.out.println("\"" + ans + "\"");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
 
 class Solution {
     // Function to reverse words in a given string
