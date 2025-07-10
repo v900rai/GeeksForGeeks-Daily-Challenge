f53e99679e6bb8fbcb8024d66be9e2b872b24da0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-10 16:00:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.java
+++ Difficulty: Easy/String Rotated by 2 Places/string-rotated-by-2-places.java
@@ -1,33 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t;
-        t = sc.nextInt();
-        String s12 = sc.nextLine();
-        for (int i = 0; i < t; i++) {
-            String s1 = sc.nextLine();
-            String s2 = sc.nextLine();
-
-            Solution obj = new Solution();
-
-            boolean flag = obj.isRotated(s1, s2);
-
-            if (flag == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 class Solution {
