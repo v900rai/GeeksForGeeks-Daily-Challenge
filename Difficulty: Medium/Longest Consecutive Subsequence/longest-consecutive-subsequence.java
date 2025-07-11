f4f62e0b4699136e14772ac513c7a14e19657c93
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-11 16:13:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -1,28 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            // Read first array
-            String input = br.readLine();
-            String[] inputArray = input.split(" ");
-            int[] arr = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
-
-            Solution ob = new Solution();
-            int res = ob.longestConsecutive(arr);
-
-            System.out.println(res);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 
