04537945fea97296bdd294a260a937b3a5ecb6cb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-11 12:10:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.java
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -1,59 +1,35 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
-        System.out.println();
-    }
-
-    // Driver program
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            new Solution().quickSort(arr, 0, n - 1);
-            printArray(arr);
+class Solution {
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high) {
+        // code here
+        if(low<high)
+        {
+        int pivotIndex=partition(arr,low,high);
+        quickSort(arr, low,pivotIndex-1);
+        quickSort(arr,pivotIndex+1,high);
         }
     }
-}
-// } Driver Code Ends
 
-
-class Solution {
-    static void quickSort(int arr[], int low, int high) {
-        if(low>=high)return;
-        int part=partition(arr,low,high);
-        quickSort(arr,low,part-1);
-        quickSort(arr,part+1,high);
-    }
     static int partition(int arr[], int low, int high) {
-        int pivot=arr[low];
-        int i=low+1;
-        int j=high;
-        while(i<=j){
-            while(i<=high && arr[i]<=pivot) i++;
-            while(j>=low && arr[j]>pivot) j--;
-            if(i<j){
+        
+        // your code here
+        int pivot=arr[high],i=low-1;
+        for(int j=low;j<high;j++)
+        {
+            if(arr[j]<arr[high])
+            {
+                i++;
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
             }
+            
         }
-        int temp=arr[j];
-        arr[j]=arr[low];
-        arr[low]=temp;
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
         
-        return j;
+        return i+1;
     }
 }
 
