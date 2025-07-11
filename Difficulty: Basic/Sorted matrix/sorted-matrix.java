7980040fbc943598c33c6f236aa68091987d45be
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-11 16:27:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted matrix/sorted-matrix.java
@@ -0,0 +1,22 @@
+// User function Template for Java
+
+class Solution {
+    int[][] sortedMatrix(int N, int Mat[][]) {
+        // code here
+        int arr[]=new int[Mat.length*Mat.length];
+        int k=0;
+        for(int i=0;i<Mat.length;i++){
+            for(int j=0;j<Mat[0].length;j++){
+                arr[k++]=Mat[i][j];
+            }
+        }
+        Arrays.sort(arr);
+        int l=0;
+        for(int i=0;i<Mat.length;i++){
+            for(int j=0;j<Mat[0].length;j++){
+                Mat[i][j]=arr[l++];
+            }
+        }
+        return Mat;
+    }
+}
