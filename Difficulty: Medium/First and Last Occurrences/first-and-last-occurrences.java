a0a3dc5da211d183fa34cebae8cc477e0d3d379b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-12 20:13:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.java
@@ -7,8 +7,7 @@ class GFG {
         // code here
         
         ArrayList<Integer> list = new ArrayList<>();
-        int first = -1;
-        int last = -1;
+        int first = -1, last = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == x) {
                 first = i;
