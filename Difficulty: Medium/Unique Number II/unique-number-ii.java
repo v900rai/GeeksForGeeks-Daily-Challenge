76f255470fe990c199336202b8614b8431f6279e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-14 10:57:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Unique Number II/unique-number-ii.java
+++ Difficulty: Medium/Unique Number II/unique-number-ii.java
@@ -8,34 +8,39 @@ import java.util.HashMap;
 // } Driver Code Ends
 
 class Solution {
-    public int[] singleNum(int[] arr) {
-        // Code here
-        int n=arr.length;
-        Map<Integer,Integer> map=new HashMap<>();
-        ArrayList<Integer> ans=new ArrayList<>();
-        
-        
-        for(int num:arr)
-        {
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        
-        for(Map.Entry <Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()==1)
-                ans.add(entry.getKey());
-        }
-        
-        Collections.sort(ans);
-        
-        int res[]=new int[ans.size()];
-        
-        for(int i=0; i<ans.size(); i++)
-        {
-            res[i]=ans.get(i);
-        }
-        
-        return res;
+   public int[] singleNum(int[] arr) {
+    // सबसे पहले array की length निकाल ली
+    int n = arr.length;
+    
+    // एक HashMap बनाया जो number की frequency को track करेगा
+    Map<Integer, Integer> map = new HashMap<>();
+    
+    // unique (single frequency वाले) elements को रखने के लिए ArrayList
+    ArrayList<Integer> ans = new ArrayList<>();
+    
+    // हर element की frequency count कर रहे हैं
+    for (int num : arr) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
     }
+    
+    // जिन elements की frequency 1 है, उन्हें list में डाल रहे हैं
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        if (entry.getValue() == 1)
+            ans.add(entry.getKey());
+    }
+    
+    // unique elements को ascending order में sort कर रहे हैं
+    Collections.sort(ans);
+    
+    // ArrayList को integer array में convert कर रहे हैं
+    int res[] = new int[ans.size()];
+    for (int i = 0; i < ans.size(); i++) {
+        res[i] = ans.get(i);
+    }
+    
+    // final result return कर रहे हैं
+    return res;
+}
 
 }
 
