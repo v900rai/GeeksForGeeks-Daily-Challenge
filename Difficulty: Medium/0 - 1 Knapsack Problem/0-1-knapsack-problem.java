3e859d09f6cd94d9e1818e232ca79ada723e0b56
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-14 22:52:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,36 +1,3 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCases = Integer.parseInt(br.readLine());
-
-        while (testCases-- > 0) {
-            int capacity = Integer.parseInt(br.readLine());
-            String[] valInput = br.readLine().split(" ");
-            String[] wtInput = br.readLine().split(" ");
-
-            int[] val = new int[valInput.length];
-            int[] wt = new int[wtInput.length];
-
-            for (int i = 0; i < valInput.length; i++) {
-                val[i] = Integer.parseInt(valInput[i]);
-            }
-
-            for (int i = 0; i < wtInput.length; i++) {
-                wt[i] = Integer.parseInt(wtInput[i]);
-            }
-
-            System.out.println(Solution.knapsack(capacity, val, wt));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 class Solution {
