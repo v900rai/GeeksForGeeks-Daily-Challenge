a29e0b31393c8cfebdcbb496e0c1447b192b91a8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-15 14:31:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
@@ -1,15 +1,25 @@
 
-
 class Solution {
     // Function to check whether there is a subarray present with 0-sum or not.
     static boolean findsum(int arr[]) {
-       HashSet <Integer> ans = new HashSet<>();
-        int prefix = 0;
-        for(int nums :arr){
-            prefix += nums;
-            if(prefix ==0|| ans.contains(prefix)) return true;
-            ans.add(prefix);
-        }
-        return false;
+         Set<Integer> set= new HashSet<>();
+         
+         int sum=arr[0];
+         set.add(sum);
+         if(sum==0) return true;
+         
+         for(int i=1; i<arr.length; i++){
+             
+             if(arr[i]==0) 
+             return true;
+             
+             sum+=arr[i];
+             
+             if(set.contains(sum) || set.contains(0)) 
+             return true;
+             else set.add(sum);
+         }
+         
+         return false;
     }
 }
