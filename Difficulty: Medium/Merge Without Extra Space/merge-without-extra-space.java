6f0964463b3a6aee09f8306fcbb2ed596d92f818
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-15 14:10:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
+++ Difficulty: Medium/Merge Without Extra Space/merge-without-extra-space.java
@@ -1,70 +1,37 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim()); // Inputting the testcases
-
-        while (t-- > 0) {
-            String arr1[] = br.readLine().trim().split(" ");
-            String arr2[] = br.readLine().trim().split(" ");
-
-            int a[] = new int[arr1.length];
-            int b[] = new int[arr2.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(arr1[i]);
-            for (int i = 0; i < b.length; i++) b[i] = Integer.parseInt(arr2[i]);
-
-            Solution ob = new Solution();
-            ob.mergeArrays(a, b);
-
-            StringBuffer str = new StringBuffer();
-            for (int i = 0; i < a.length; i++) {
-                str.append(a[i] + " ");
-            }
-            System.out.println(str);
-            str = new StringBuffer();
-            for (int i = 0; i < b.length; i++) {
-                str.append(b[i] + " ");
-            }
-            System.out.println(str);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
 
+
+
 class Solution {
-    // Function to merge the arrays.
+    // Function to merge two sorted arrays without using extra space
     public void mergeArrays(int a[], int b[]) {
-        // code here
         int n = a.length;
         int m = b.length;
-        int left = n-1;
-        int right = 0;
-        while(left >= 0 && right < m) {
-            if(a[left] > b[right]) {
-                //swap
+
+        int left = n - 1;  // Pointer at the end of array 'a'
+        int right = 0;     // Pointer at the beginning of array 'b'
+
+        // Step 1: Swap elements to make sure all smaller elements are in 'a' and larger in 'b'
+        while (left >= 0 && right < m) {
+            if (a[left] > b[right]) {
+                // If a's element is greater, swap it with b's element
                 int temp = a[left];
                 a[left] = b[right];
                 b[right] = temp;
                 left--;
                 right++;
-            } else { 
-                //a[left] < b[right]
+            } else {
+                // If no more swapping needed, exit loop
                 break;
             }
         }
+
+        // Step 2: Sort both arrays individually
         Arrays.sort(a);
         Arrays.sort(b);
     }
 }
+
 
