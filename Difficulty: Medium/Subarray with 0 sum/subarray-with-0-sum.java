b1a014058706645a956f4383e7f6528ab1d6f380
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-15 14:22:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
+++ Difficulty: Medium/Subarray with 0 sum/subarray-with-0-sum.java
@@ -1,29 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t > 0) {
-            String[] inputLine;
-            inputLine = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            t--;
-
-            System.out.println(new Solution().findsum(arr) == true ? "true" : "false");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 class Solution {
