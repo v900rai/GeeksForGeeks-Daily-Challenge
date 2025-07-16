6885c927917f4d4e331f133624b13f9c9be16ba4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-16 10:53:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nine Divisors/nine-divisors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int countNumbers(int n) {
+        // code here
+          // Code Here
+        int ans = 0;
+        for(int i=6;i*i<=n;i++) {
+            int x = i*i,cnt = 1;
+            for(int j=2;j*j<x;j++) {
+                if(x%j==0) cnt++;
+                if(cnt>4) break;
+            }
+            if(cnt==4) ans++;
+        }
+        return ans;
+    
+    }
+}
