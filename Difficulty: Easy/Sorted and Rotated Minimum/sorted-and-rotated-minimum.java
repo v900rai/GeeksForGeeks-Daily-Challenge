6ba3ef1a1f1037674fdc2e676e4afb3c9e83bd50
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-19 20:10:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
+++ Difficulty: Easy/Sorted and Rotated Minimum/sorted-and-rotated-minimum.java
@@ -1,41 +1,19 @@
-//{ Driver Code Starts
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = Integer.parseInt(scanner.nextLine());
-
-        Solution ob = new Solution(); // Instantiate the Solution object once
-
-        while (t-- > 0) {
-            String line = scanner.nextLine();
-            String[] elements = line.split(" ");
-            int[] arr = new int[elements.length]; // Changed to int[]
-
-            for (int i = 0; i < elements.length; i++) {
-                arr[i] = Integer.parseInt(elements[i]);
-            }
-
-            System.out.println(ob.findMin(arr));
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
 // User function Template for Java
 
-class Solution {
-    public int findMin(int[] arr) {
-        int min=arr[0];
-        for(int i=1; i<arr.length; i++){
-            min=Math.min(arr[i],min);
+
+
+class Solution {  
+    public int findMin(int[] arr) { 
+        int low=0;
+        int high=arr.length-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(arr[mid]>arr[high]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
         }
-        return min;
-        
+        return arr[low];
     }
 }
