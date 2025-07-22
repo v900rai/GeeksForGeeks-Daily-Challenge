ed7c8a6c351974bda3e1dbf725344a39d18e7736
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-22 15:51:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -1,35 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim());
-
-        while (t-- > 0) {
-            String line = read.readLine().trim();
-            String[] numsStr = line.split(" ");
-            int[] nums = new int[numsStr.length];
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
-            }
-
-            int d = Integer.parseInt(read.readLine().trim());
-
-            Solution ob = new Solution();
-            ArrayList<Integer> result = ob.subarraySum(nums, d);
-            // Print all elements in the result list
-            for (int i : result) {
-                System.out.print(i + " ");
-            }
-            System.out.println(); // Print a new line after the result
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 class Solution {
     static ArrayList<Integer> subarraySum(int[] arr, int target) {
@@ -43,7 +11,7 @@ class Solution {
 
             // अगर currentSum टारगेट से बड़ा हो जाता है, तो शुरुआत से घटाएं
             while (currentSum > target && start < end) {
-                currentSum -= arr[start];
+                currentSum =currentSum - arr[start];
                 start++;
             }
 
@@ -52,6 +20,7 @@ class Solution {
                 result.add(start + 1); // 1-आधारित इंडेक्सिंग के लिए
                 result.add(end + 1);
                 return result;
+                
             }
         }
 
