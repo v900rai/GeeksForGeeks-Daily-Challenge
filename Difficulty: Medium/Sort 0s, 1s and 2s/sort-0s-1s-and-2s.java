3029617040128c807046551d2e04f0bdc1d2dffc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-22 16:15:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,34 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String input = br.readLine();
-            String[] inputArray = input.split("\\s+");
-            int a[] = new int[inputArray.length];
-
-            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
-
-            Solution ob = new Solution();
-            ob.sort012(a);
-
-            for (int num : a) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-        }
-    }
-}
-
-
-// } Driver Code Ends
 
 class Solution {
    public void sort012(int[] arr) {
@@ -66,6 +35,3 @@ class Solution {
 
 
 
-
-//{ Driver Code Starts.
-// } Driver Code Ends
