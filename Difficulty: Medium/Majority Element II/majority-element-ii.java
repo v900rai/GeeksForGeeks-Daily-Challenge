2f9abda0ded2b9ecdb832393e457e8e7f4f1e67d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-07-26 10:14:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element II/majority-element-ii.java
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -1,51 +1,18 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // Consume the newline character
-
-        while (t-- > 0) {
-            String s = sc.nextLine();
-            String[] parts = s.split(" ");
-            int[] nums = new int[parts.length];
-            for (int i = 0; i < parts.length; i++) {
-                nums[i] = Integer.parseInt(parts[i]);
-            }
-            Solution ob = new Solution();
-            List<Integer> ans = ob.findMajority(nums);
-
-            if (ans.size() == 0) {
-                System.out.println("[]");
-            } else {
-                for (int i : ans) {
-                    System.out.print(i + " ");
-                }
-                System.out.println();
-            }
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
 class Solution {
-    public List<Integer> findMajority(int[] nums) {
-        int n=nums.length;
-        List<Integer> res=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        int tot = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        TreeMap<Integer,Integer> mp = new TreeMap<>();
+        
+        for(int n:arr){
+            mp.put(n,mp.getOrDefault(n,0)+1);
         }
-        for(int key:map.keySet()){
-            if(map.get(key)>n/3){
-                res.add(key);
-            }
+        
+        for(int k:mp.keySet()){
+            if(tot/3 < mp.get(k)) ans.add(k);
         }
-        return res;
+        
+        return ans;
     }
 }
