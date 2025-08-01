9e29faad8f62860c5a6e6eda87712cbfd8deb7ca
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-01 17:52:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.java
+++ Difficulty: Medium/Majority Element/majority-element.java
@@ -8,9 +8,12 @@
 class Solution {
     static int majorityElement(int arr[]) {
         if(arr.length==1)
-        return arr[0];
+        return arr[0]; //base case
+        
+        
         int size=arr.length/2;
         int count=1;
+        
         Arrays.sort(arr);
         for(int i=1;i<arr.length;i++){
             if(arr[i]==arr[i-1]){
