b6590c6f3984bb58c43c69847f20ab8a097bb599
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-02 22:30:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -2,21 +2,23 @@
 class Solution {
     static ArrayList<Integer> subarraySum(int[] arr, int target) {
         ArrayList<Integer> result = new ArrayList<>();
+        
         int n = arr.length;
-        int currentSum = 0;
-        int start = 0;
+        int sum = 0;
+        int start =0;
+        
 
         for (int end = 0; end < n; end++) {
-            currentSum =currentSum+ arr[end];
+          sum+=arr[end];
 
             // अगर currentSum टारगेट से बड़ा हो जाता है, तो शुरुआत से घटाएं
-            while (currentSum > target && start < end) {
-                currentSum =currentSum - arr[start];
+            while (sum > target && start < end) {
+                sum =sum - arr[start];
                 start++;
             }
 
             // अगर currentSum टारगेट के बराबर हो जाए
-            if (currentSum == target) {
+            if(sum == target) {
                 result.add(start + 1); // 1-आधारित इंडेक्सिंग के लिए
                 result.add(end + 1);
                 return result;
