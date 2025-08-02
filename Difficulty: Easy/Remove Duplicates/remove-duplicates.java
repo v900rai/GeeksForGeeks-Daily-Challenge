7fbf9ec56023f1dc07dbb8b465ba079d36fd2e9a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-02 22:33:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates/remove-duplicates.java
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.java
@@ -1,24 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-
-            Solution ob = new Solution();
-            String result = ob.removeDups(s);
-
-            System.out.println(result);
-        }
-    }
-}
-// } Driver Code Ends
 
 
 
