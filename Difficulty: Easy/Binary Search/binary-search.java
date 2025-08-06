9e46192022340255f4f401160b6d59c637f182e3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-06 10:53:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Binary Search/binary-search.java
+++ Difficulty: Easy/Binary Search/binary-search.java
@@ -1,69 +1,23 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class GFG {
-    public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        sc.nextLine(); // consume the newline
-        while (t-- > 0) {
-            int k = sc.nextInt();
-            sc.nextLine(); // consume the newline
-            String input = sc.nextLine();
-            String[] strNumbers = input.split(" ");
-            int[] arr = new int[strNumbers.length];
-            for (int i = 0; i < strNumbers.length; i++) {
-                arr[i] = Integer.parseInt(strNumbers[i]);
-            }
-            Solution ob = new Solution();
-            int res = ob.binarysearch(arr, k);
-            System.out.println(res);
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
-
-
-// User function Template for Java
 class Solution {
     public int binarysearch(int[] arr, int k) {
         // Code Here
-        
-          
-        int left=0;
-        int right= arr.length-1;
-       int result = -1;
-        
-        while(left<=right)
-        {
-            int mid = left + (right-left)/2;
-            if(arr[mid] == k)
-            {
-              result = mid;
-              right = mid-1;
+        int start = 0;
+        int end = arr.length-1;
+        int result = -1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==k){
+                result =  mid;
+                end = mid-1;
             }
-            else if(arr[mid]>k)
-            {
-                right = mid-1;
+            else if(arr[mid]<k){
+                start = mid+1;
             }
-            else
-            {
-                left= mid+1;
+            else{
+                end = mid-1;
             }
-            
         }
-        return result ;
+        return result;
     }
 }
 
