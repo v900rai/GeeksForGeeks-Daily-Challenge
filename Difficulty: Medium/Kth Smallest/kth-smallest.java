51714c9eac547efe300997cb60902201fd7f4f07
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-06 10:51:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,18 +1,10 @@
-
-
 // User function Template for Java
 
 class Solution {
     public static int kthSmallest(int[] arr, int k) {
-     
-     Arrays.sort(arr);
-     int count=0;
-     for(int i=0; i<k; i++){
-         count=arr[i];
-         
-     }
-     return count;
-    
+        // Your code here
+        Arrays.sort(arr);
+        return arr[k-1];
+        // time complexity O(nlog n)
     }
-    
 }
