c9a299b5b450e1147a86b344100f4ad1da9f1535
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-08 11:31:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -1,15 +1,13 @@
 class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
-        ArrayList<Integer> list=new ArrayList<>();
-           int currentSum=arr[arr.length-1];
-         for(int i=arr.length-1; i>=0; i--){
-             if(currentSum<=arr[i]){
-                 list.add(0 ,arr[i]);
-                 currentSum=arr[i];
-             }
-         }
-         return list;
-        
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        int currentSum = arr[arr.length - 1]; // last element is always a leader
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (currentSum <= arr[i]) {
+                list.add(0, arr[i]); // insert at front to maintain order
+                currentSum = arr[i];
+            }
+        }
+        return list;
     }
 }
