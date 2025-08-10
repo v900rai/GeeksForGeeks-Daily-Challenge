19cbbbb0567450e28f7124c16d201ad073944113
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-10 10:09:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Palindrome SubStrings/palindrome-sub-strings.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int countPS(String s) {
+        // code here
+        int n = s.length();
+        int count = 0;
+        int left, right = 0;
+        for(int i=0;i<n;i++) {
+            left = i-1; right = i;
+            while(left>=0 && right<n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
+            }
+            left = i - 1;
+            right = i + 1;
+            while(left>=0 && right<n && s.charAt(left) == s.charAt(right)) {
+                count++;
+                left--;
+                right++;
+            }
+        }
+        return count;
+    }
+}
