1965d02c39748f98f5ed0795d0a519b0bb9d2d36
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-13 16:19:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-
-        // Reading total number of testcases
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            // reading the string
-            String st = sc.next();
-
-            // calling ispar method of Paranthesis class
-            // and printing "balanced" if it returns true
-            // else printing "not balanced"
-            if (new Solution().isBalanced(st) == true)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 
