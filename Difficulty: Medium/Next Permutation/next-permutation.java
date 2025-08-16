ab684c5d2f8efcbfa4be8d72609e39184d332689
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-16 10:50:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.java
+++ Difficulty: Medium/Next Permutation/next-permutation.java
@@ -1,10 +1,3 @@
-//{ Driver Code Starts
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-
-
-// } Driver Code Ends
 // User function Template for Java
 
 class Solution {
@@ -54,31 +47,3 @@ class Solution {
 }
 
 
-
-//{ Driver Code Starts.
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine()); // Number of test cases
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int[] arr = new int[inputLine.length];
-            for (int i = 0; i < inputLine.length; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            int N = arr.length;
-            Solution ob = new Solution();
-            ob.nextPermutation(arr); // Generate the next permutation
-            StringBuilder out = new StringBuilder();
-            for (int i = 0; i < N; i++) {
-                out.append(arr[i] + " "); // Corrected: use arr[i] instead of arr.get(i)
-            }
-            System.out.println(out.toString().trim()); // Print the output
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
