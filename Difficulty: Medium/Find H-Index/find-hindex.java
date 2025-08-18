74feaef3426ce45fb2fedb40ccf5bf547b6ed490
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-18 10:21:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,57 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_cases = Integer.parseInt(br.readLine().trim());
-
-        while (test_cases-- > 0) {
-            // Read the array from input line
-            String[] input = br.readLine().trim().split(" ");
-            int[] arr = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-            }
-
-            // Solution instance to invoke the function
-            Solution ob = new Solution();
-            int result = ob.hIndex(arr);
-
-            System.out.println(result);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    // Function to find hIndex
-    public int hIndex(int[] citations) {
-        int[] freq= new int [citations.length+1];
-        for(int i: citations){
-            if(i>freq.length-1){
-                freq[freq.length-1]++;
-            }
-            else{
-                freq[i]++;
-            }
+    public int hIndex(int[] nums) {
+        // code here
+        int n = nums.length;
+        int max = nums[0];
+        for(int i=0;i<n;i++) max = Math.max(nums[i],max);
+        
+        int [] pref = new int[max+1];
+        
+        for(int i=0;i<n;i++){
+            pref[nums[i]]++;
         }
-        //sum ko calculate krna h 
-        int sum=0;
-        for(int i=freq.length-1; i>=0; i--){
-            sum+=freq[i];
-            if(sum>=i){
-                return i;
-            }
+        
+        for(int i=max-1;i>=0;i--){
+            pref[i]+=pref[i+1];
+        }
+        
+        for(int i=max;i>=0;i--){
+            if(pref[i]>=i) return i;
         }
         return 0;
-       
     }
 }
+ 
