b889535a898c425e0cec717798d6fa7fdbd2fc85
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-22 10:57:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stickler Thief/stickler-thief.java
+++ Difficulty: Medium/Stickler Thief/stickler-thief.java
@@ -1,48 +1,14 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            // calling method findMaxSum() of class solve
-            System.out.println(new Solution().findMaxSum(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-
 class Solution {
     public int findMaxSum(int arr[]) {
+        // code here
         int n = arr.length;
-        if (n == 0) return 0;
-        if (n == 1) return arr[0];
-
-        int prev2 = 0, prev1 = arr[0];
-
-        for (int i = 1; i < n; i++) {
-            int pick = arr[i] + prev2;
-            int notPick = prev1;
-            int curr = Math.max(pick, notPick);
-            
-            prev2 = prev1;
-            prev1 = curr;
+        int prev_prev = 0;
+        int prev = 0;
+        for(int i=0; i<n; i++){
+            int curr = Math.max(prev, prev_prev+arr[i]);
+            prev_prev = prev;
+            prev = curr;
         }
-        
-        return prev1;
+        return prev;
     }
 }
