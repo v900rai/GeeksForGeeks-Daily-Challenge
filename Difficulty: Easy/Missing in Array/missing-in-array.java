97a48af3f4564c4178f4b32c839e80b291bfcddb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-26 09:42:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,12 +1,13 @@
 class Solution {
     int missingNum(int arr[]) {
-        Arrays.sort(arr);
-        int count=1;
-        for(int i=0; i<arr.length; i++){
-            if(count == arr[i])
-            count++;
-        }
-        return count;
+       Arrays.sort(arr);
+       int count =1;
+       for(int i=0; i<arr.length; i++){
+           if(count == arr[i]){
+               count++;
+           }
+       }
         
+        return count;
     }
 }
