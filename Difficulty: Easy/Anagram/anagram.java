5d1a4ff6bf261a4c6726a965929551c58a94cc98
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-08-29 10:09:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,19 +1,10 @@
-
-
 class Solution {
-    // Function is to check whether two strings are anagram of each other or not.
     public static boolean areAnagrams(String s1, String s2) {
-        if(s1.length()!=s2.length()){
-            return false;
-        }
-
-       char onechar [] =s1.toCharArray();
-       char twochar [] =s2.toCharArray();
-       
-       Arrays.sort(onechar);
-       Arrays.sort(twochar);
-       
-        return Arrays.equals(onechar, twochar);
-        
+        char c1[] = s1.toCharArray();
+        char c2[] = s2.toCharArray();
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        return Arrays.equals(c1,c2);
     }
 }
+
