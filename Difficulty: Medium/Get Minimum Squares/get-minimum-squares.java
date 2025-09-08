e956dd64d7d69c3b044bafe0211b7e373393dfa2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-08 17:50:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Get Minimum Squares/get-minimum-squares.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+    public int MinSquares(int n) {
+        // Code here
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+         for(int i =1; i <= n; i++) {
+            dp[i] = i;
+            for(int j =1; j* j <= i; j++) {
+                dp[i] = Math.min(dp[i],  1 + dp[i - (j*j)]); 
+            }
+        }
+        return dp[n];
+    }
+}
