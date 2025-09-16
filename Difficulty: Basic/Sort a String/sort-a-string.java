3d475dabc040badb390a723a3acda83e6a3067a1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-16 16:18:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sort a String/sort-a-string.java
+++ Difficulty: Basic/Sort a String/sort-a-string.java
@@ -1,41 +1,9 @@
-//{ Driver Code Starts
-//Initial Template for Java
-import java.io.*;
-import java.util.*; 
-
-class GFG{
-    public static void main(String args[]) throws IOException { 
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine().trim());
-        while(t-- > 0){
-            String s = sc.nextLine().trim();
-            Solution obj = new Solution();
-            System.out.println(obj.sort(s));
-        
-System.out.println("~");
-}
+// User function Template for Java
+class Solution {
+    String sort(String s) {
+        // code here
+        char[] ch=s.toCharArray();
+        Arrays.sort(ch);
+        return new String(ch);
     }
 }
-// } Driver Code Ends
-
-
-//User function Template for Java
-class Solution 
-{ 
-    String sort(String s) 
-    {
-        
-        char a[]=new char[s.length()];
-        for(int i=0;i<a.length;i++)
-        {
-            a[i]=s.charAt(i);
-        }
-        Arrays.sort(a);
-        String res="";
-        for(int i=0;i<a.length;i++)
-        {
-            res+=a[i];
-        }
-        return res;
-    }
-} 
