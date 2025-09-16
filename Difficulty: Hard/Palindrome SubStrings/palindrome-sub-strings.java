3d56a5aa5227b64957cbec2ba48849882737cb0d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-16 10:01:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindrome SubStrings/palindrome-sub-strings.java
+++ Difficulty: Hard/Palindrome SubStrings/palindrome-sub-strings.java
@@ -3,7 +3,8 @@ class Solution {
         // code here
         int n = s.length();
         int count = 0;
-        int left, right = 0;
+        int left;
+        int right = 0;
         for(int i=0;i<n;i++) {
             left = i-1; right = i;
             while(left>=0 && right<n && s.charAt(left) == s.charAt(right)) {
