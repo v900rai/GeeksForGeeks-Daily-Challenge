99080086e5cfebfe6b34857a970ddd4cc0ce2599
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-17 18:34:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,53 +1,40 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     static String decodeString(String s) {
+        // code here
         Stack<Integer> countStack = new Stack<>();
-        Stack<String> stringStack = new Stack<>();
-        String currentString = "";
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
         int k = 0;
 
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                k = k * 10 + (ch - '0');
-            } else if (ch == '[') {
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+               
+                k = k * 10 + (c - '0');
+            } else if (c == '[') {
+               
                 countStack.push(k);
-                stringStack.push(currentString);
-                currentString = "";
+                stringStack.push(current);
+                current = new StringBuilder();
                 k = 0;
-            } else if (ch == ']') {
-                StringBuilder decodedString = new StringBuilder(stringStack.pop());
-                int repeatTimes = countStack.pop();
-                for (int i = 0; i < repeatTimes; i++) {
-                    decodedString.append(currentString);
+            } else if (c == ']') {
+                
+                int repeat = countStack.pop();
+                StringBuilder decoded = stringStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    decoded.append(current);
                 }
-                currentString = decodedString.toString();
+                current = decoded;
             } else {
-                currentString += ch;
+               
+                current.append(c);
             }
         }
-        return currentString;
+
+        return current.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decodeString("3[b2[ca]]")); 
+        System.out.println(decodeString("3[ab]"));
     }
 }
