57b3fa46a1934c68a79f80cabc421f2c60ebc03e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-24 11:56:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Linked List/print-linked-list.java
+++ Difficulty: Basic/Print Linked List/print-linked-list.java
@@ -1,56 +1,4 @@
-//{ Driver Code Starts
-import java.util.*;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int x) {
-        data = x;
-        next = null;
-    }
-}
-
-class LinkedList {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int t = scanner.nextInt();
-        scanner.nextLine(); // Consume newline character after t
-
-        while (t-- > 0) {
-            String input =
-                scanner.nextLine(); // Read the entire line for the array elements
-            Scanner lineScanner = new Scanner(input);
-            Node head = null, tail = null;
-            int x;
-
-            // Building the linked list from input
-            while (lineScanner.hasNextInt()) {
-                x = lineScanner.nextInt();
-                if (head == null) {
-                    head = new Node(x);
-                    tail = head;
-                } else {
-                    tail.next = new Node(x);
-                    tail = tail.next;
-                }
-            }
-            lineScanner.close();
-
-            Solution ob = new Solution();
-            ob.printList(head);
-            System.out.println();
-
-            System.out.println("~");
-        }
-        scanner.close();
-    }
-}
-
-// } Driver Code Ends
-
-
-/* Node is defined as
+/*
 class Node {
     int data;
     Node next;
@@ -59,21 +7,18 @@ class Node {
         next = null;
     }
 }*/
-/*
-    Print elements of a linked list on console
-    Head pointer input could be NULL as well for empty list
-*/
 
 class Solution {
-    // Function to display the elements of a linked list in same line
-    void printList(Node head) {
+    public ArrayList<Integer> printList(Node head) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>(); 
+        return rec(head,list);
+    }
+    static ArrayList<Integer> rec(Node head,ArrayList<Integer>list){
         if(head==null){
-            return ;
+            return list;
         }
-        while(head != null){
-            System.out.print(head.data+" ");
-            head=head.next;
-        }
-     
+        list.add(head.data);
+        return rec(head.next,list);
     }
 }
