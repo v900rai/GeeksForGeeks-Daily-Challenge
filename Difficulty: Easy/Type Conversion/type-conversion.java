9fda89043a6f0eab50112a1e34f222b54e023e2b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 10:27:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Type Conversion/type-conversion.java
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -1,44 +1,7 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-
-
-import java.io.*;
-
-class GFG{
-    
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static int typeCast(double d){
-    // code here
-    int a=(int)d;
-    return a;
-    
-    
-}
-
-
-//{ Driver Code Starts.
-
-    
-    public static void main(String args[]) throws IOException{
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        
-        int t = Integer.parseInt(read.readLine());
-        
-        while(t-- > 0){
-            double d = Double.parseDouble(read.readLine());
-            
-            int answer = typeCast(d);
-            
-            System.out.println(answer);
-        
-System.out.println("~");
-}
-        
+class Solution {
+    public static int typeCast(double d) {
+        // code here
+         // code here
+        return (int)d;
     }
 }
-// } Driver Code Ends
