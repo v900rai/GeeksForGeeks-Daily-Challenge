4bda3951f0f4bba6dd359cc5f2b71d4c7805fa17
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 10:51:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print With Space/print-with-space.java
+++ Difficulty: Basic/Print With Space/print-with-space.java
@@ -1,35 +1,8 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.Scanner;
-
-class Main {
-    static Scanner scn = new Scanner(System.in);
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-
-public static void utility() {
-    String a = scn.nextLine();
-    String b = scn.nextLine();
-    // Write your code below that prints a <space> b
-   System.out.println(a+" "+b);
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
-        int t = scn.nextInt();
-        scn.nextLine();
-        while (t-- > 0) {
-            utility();
-
-            System.out.println("~");
-        }
+public class Solution {
+    public void utility(Scanner scn) {
+        // code here
+        String a=scn.next();
+        String b=scn.next();
+        System.out.println(a+" "+b);
     }
 }
-
-// } Driver Code Ends
