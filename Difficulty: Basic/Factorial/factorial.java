422c60d8a0627c5fd3efd3679235b910cfa4e188
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 12:44:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,40 +1,12 @@
-//{ Driver Code Starts
-//Initial Template for Java
 
-import java.util.Scanner;
+// User function Template for Java
+class Solution {
+    public static int nFactorial(int n) {
+        int ans = 1;
+        for(int i=1;i<=n;i++){
+            ans*=i;
+        }
 
-class Main {
-
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static int nFactorial(int n){
-    int ans = 1;
-    
-    //Write your code here
-    for(int i=1; i<=n; i++){
-        ans=ans*i;
-    }
-    return ans;
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n = scn.nextInt();
-            int ans = nFactorial(n);
-            System.out.println(ans);
-        
-System.out.println("~");
-}
-        scn.close();
+        return ans;
     }
 }
-// } Driver Code Ends
