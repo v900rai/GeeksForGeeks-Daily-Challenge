4fa6cf2ea3c86cd3bcc34908bc4aea85ecec64e3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 14:13:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Table Difference/table-difference.java
+++ Difficulty: Basic/Table Difference/table-difference.java
@@ -1,40 +1,14 @@
-//{ Driver Code Starts
-//Initial Template for Java
+import java.util.*;
 
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static void difference(int n1, int n2){
-  for(int i=1; i<=10; i++){
-      int vishal=(n1*i)-(n2*i);
-      System.out.print(vishal+" ");
-  }
-   
-    
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while(t-- > 0) {
-            int n1 = scn.nextInt();
-            int n2 = scn.nextInt();
-            difference(n1, n2);
-            System.out.println();
-        
-System.out.println("~");
-}
-        scn.close();
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner s =new Scanner(System.in);
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        int diff=n1-n2;
+        for(int i=1;i<=10;i++){
+            System.out.print(diff*i+" ");
+        }
     }
 }
-// } Driver Code Ends
