8abd800af99341b37ff8109a97c29e21fd28ab9c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 10:35:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment - Java/learn-to-comment-java.java
@@ -0,0 +1,21 @@
+
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Prints 
+        System.out.println(a);
+        // Prints 
+        //System.out.println(b); not comment on b acording problem
+        // Prints
+        System.out.println(c);
+    }
+}
+
