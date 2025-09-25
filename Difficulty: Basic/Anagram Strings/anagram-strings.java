807b8a91e97f36034bb224ec1b57278ab73755f0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 13:20:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram Strings/anagram-strings.java
+++ Difficulty: Basic/Anagram Strings/anagram-strings.java
@@ -1,24 +1,3 @@
-//{ Driver Code Starts
-// Initial template for Java
-import java.util.*;
-import java.io.*;
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S1 = read.readLine();
-            String S2 = read.readLine();
-            Solution ob = new Solution();
-
-            System.out.println(ob.areAnagram(S1, S2));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
 
 
 // User function template for Java
