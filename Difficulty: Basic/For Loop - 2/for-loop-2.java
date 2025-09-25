22ee2576b92ce905c3d5fd241e3c0c6072424126
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-25 12:49:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/For Loop - 2/for-loop-2.java
+++ Difficulty: Basic/For Loop - 2/for-loop-2.java
@@ -1,45 +1,10 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-//User function Template for Java
-
-public static void utility(String s){
-    // Length of string is given by s.length()
-    // character at any index i is given by s.charAt(i)
-    
-    //Write your code here
-     for(int i = 0;i<s.length();i++)
-    {
-        if(i%2==0)
-        {
-            System.out.print(s.charAt(i));
+class Solution {
+    public void printEvenIndices(String s) {
+        // code here
+        for(int i=0; i<=s.length(); i++){
+            if(i%2==0){
+                System.out.print(s.charAt(i));
+            }
         }
     }
 }
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args)
-    {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        scn.nextLine();
-        while(t-- > 0) {
-            String s = scn.nextLine();
-            utility(s);
-            System.out.println();
-        
-System.out.println("~");
-}
-        scn.close();
-    }
-}
-// } Driver Code Ends
