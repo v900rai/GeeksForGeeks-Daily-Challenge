79149151901d571d252bd08ec562214947e8b0d5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-26 14:15:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.java
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine().trim()); // Read number of test cases
-
-        while (t-- > 0) {
-            String line = read.readLine().trim(); // Read the array input
-            String[] numsStr = line.split(" ");   // Split the input string by spaces
-            int[] nums =
-                new int[numsStr.length]; // Convert string array to integer array
-            for (int i = 0; i < numsStr.length; i++) {
-                nums[i] = Integer.parseInt(numsStr[i]);
-            }
-
-            int k = Integer.parseInt(read.readLine().trim()); // Read target sum
-
-            Solution ob = new Solution();
-            int ans = ob.longestSubarray(nums, k); // Call the function and store result
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 
