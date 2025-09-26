5f927c71095cc7f1656d83beb4574c3755804449
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-26 11:35:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Concatenate Integers/concatenate-integers.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+          int c = a*10+b;
+        System.out.println(c);
+        // Write Your Code to Concatenate and print
+        // Write Your Code to Concatenate and print
+    }
+}
