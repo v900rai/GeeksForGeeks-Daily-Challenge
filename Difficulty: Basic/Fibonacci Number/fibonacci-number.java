9a40fc100c4a438617c445259746161b2657d9c1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-26 12:09:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Fibonacci Number/fibonacci-number.java
+++ Difficulty: Basic/Fibonacci Number/fibonacci-number.java
@@ -1,25 +1,11 @@
-import java.util.*;
-
-public class Solution {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a = 0, b = 1, res = 0;
-        if(n == 0){
-            System.out.println("0");
-            
-        }
-        if(n == 1){
-            System.out.println("1");
-        }
-        else{
-            for(int i = 2; i <= n; i ++){
-                res = a + b;
-                a = b;
-                b = res;
-            }
-            System.out.println(res);
-        }
+class Solution {
+    public static int fibonacci(int n) {
+        // Write your code here to calculate
+        // to calculate the nth fibonacci number
+          if(n ==0) return 0;
+        if(n==1) return 1;
+        
+        return fibonacci(n-1) + fibonacci(n-2);
+        
     }
 }
