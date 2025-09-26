7c42a0f862569261a79932b1cc15ebef840039a7
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-26 11:41:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square/print-square.java
+++ Difficulty: Basic/Print Square/print-square.java
@@ -1,48 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for Java
+import java.util.*;
 
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-
-public static void square(int s) {
-   for(int i = 0;i<s;i++)
-    {
-        for(int j = 0;j<s;j++)
-        {
-            if(i==0||j==0||j==s-1||i==s-1)
-            {
-                System.out.print("* ");
+public class Solution {
+    public static void solve() {
+        // Your Code Here
+        Scanner s = new Scanner(System.in);
+        int n =s.nextInt();
+        
+                 for (int i = 1;i<=n;i++){
+            for(int j = 1;j<=n;j++){
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                }else{
+                    System.out.print("  ");
+                }
             }
-            else
-            {
-                System.out.print("  ");
-            }
+            System.out.println();
         }
-        System.out.println();
-
-
-}
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while (t-- > 0) {
-            int s = scn.nextInt();
-            square(s);
-
-            System.out.println("~");
-        }
-        scn.close();
     }
 }
-// } Driver Code Ends
+
+ 
