bfe0904f0121d7bcbae887da43e49319557dbe65
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-29 19:52:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.java
+++ Difficulty: Basic/Convert String to LowerCase/convert-string-to-lower-case.java
@@ -1,25 +1,3 @@
-//{ Driver Code Starts
-// Initial template for Java
-
-import java.util.*;
-import java.io.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
-
-            System.out.println(ob.toLower(s));
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
 
 
 // User function template for Java
