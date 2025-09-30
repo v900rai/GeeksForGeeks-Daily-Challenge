9655df77ee57d12b5a95a4b05265557fc584dcd4
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-30 11:56:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse digits/reverse-digits.java
+++ Difficulty: Basic/Reverse digits/reverse-digits.java
@@ -1,26 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            Solution ob = new Solution();
-            int ans = ob.reverseDigits(n);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
