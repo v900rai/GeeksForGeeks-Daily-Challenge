ad0b5d37b37f654825bb17617b2e578d106260e5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-30 14:02:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome/palindrome.java
+++ Difficulty: Easy/Palindrome/palindrome.java
@@ -1,25 +1,3 @@
-//{ Driver Code Starts
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // Number of test cases
-
-        Solution ob = new Solution();
-
-        while (T-- > 0) {
-            int n = sc.nextInt();
-            boolean ans = ob.isPalindrome(n);
-            System.out.println(ans ? "true" : "false");
-            System.out.println("~");
-        }
-
-        sc.close();
-    }
-}
-
-// } Driver Code Ends
 
 
 
