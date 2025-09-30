e906f40d3d29b827010be5c5213bfc84687f6b2f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-09-30 11:20:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -1,31 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Driverclass {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t-- > 0) {
-            String st = sc.next();
-
-            char ans = new Solution().nonRepeatingChar(st);
-
-            if (ans != '$')
-                System.out.println(ans);
-            else
-                System.out.println(-1);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 
@@ -36,9 +8,14 @@ class Solution {
         Map<Character, Integer> freqMap = new HashMap<>();
 
         // Step 1: हर character की frequency count करो
-        for (int i = 0; i < S.length(); i++) {
-            char ch = S.charAt(i);
-            freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        // for (int i = 0; i < S.length(); i++) {
+        //     char ch = S.charAt(i);
+        //     freqMap.put(ch, freqMap.getOrDefault(ch, 0) + 1);
+        // }
+        
+        for(int i=0; i<S.length(); i++){
+            char ch=S.charAt(i);
+            freqMap.put(ch, freqMap.getOrDefault(ch,0)+1);
         }
 
         // Step 2: फिर से traverse करके पहला non-repeating character ढूंढो
