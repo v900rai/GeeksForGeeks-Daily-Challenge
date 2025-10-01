dbb107b89b0a2dbb38867b43d3c13a11e7a6336b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-01 11:00:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Container With Most Water/container-with-most-water.java
+++ Difficulty: Medium/Container With Most Water/container-with-most-water.java
@@ -1,25 +1,14 @@
-
-
-// User function Template for Java
-
 class Solution {
     public int maxWater(int arr[]) {
-           
-        int left =0;
-        int right = arr.length-1;
-        int maxWater = 0;
+        // Code Here
+        int left=0;
+        int right=arr.length-1;
+        int maxWater=0;
         while(left<right){
-            int width = right-left;
-            // width niksl diya hu 
-            
-            int height = Math.min(arr[left],arr[right]);
-            // heigth bhi niksl gys h 
-            
-            int currentWater = width*height;
-            // current watter bhi naikal diya hu 
-            
-            maxWater = Math.max(maxWater,currentWater);
-            // maximum watter bhi nikal diya hu 
+            int width=right-left;
+            int height=Math.min(arr[left],arr[right]);
+            int currentWater=width*height;
+            maxWater=Math.max(maxWater,currentWater);
             
             if(arr[left]<arr[right]) 
             left++;
@@ -29,6 +18,6 @@ class Solution {
             right--;
         }
         return maxWater;
-        
-    }
+        }
+    
 }
