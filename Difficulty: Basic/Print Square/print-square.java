22e986153e8a3827224076076664565818bb9c22
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-01 23:57:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Square/print-square.java
+++ Difficulty: Basic/Print Square/print-square.java
@@ -6,7 +6,7 @@ public class Solution {
         Scanner s = new Scanner(System.in);
         int n =s.nextInt();
         
-                 for (int i = 1;i<=n;i++){
+           for (int i = 1;i<=n;i++){
             for(int j = 1;j<=n;j++){
                 if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("* ");
