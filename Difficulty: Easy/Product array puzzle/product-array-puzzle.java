520d4310dc3ea6e408783efccdd6bf4406ded72a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-01 10:25:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.java
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.java
@@ -1,35 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            Solution ob = new Solution();
-            int[] ans = new int[n];
-            ans = ob.productExceptSelf(arr);
-
-            for (int i = 0; i < n; i++) {
-                System.out.print(ans[i] + " ");
-            }
-            System.out.println();
-            System.out.println("~");
-            t--;
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
