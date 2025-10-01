8dc8470fa4eaf49f4a5378eb018ab30d22cc9212
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-01 23:57:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
@@ -1,39 +1,11 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-public static int arraySum(int[] numbers) {
-       int sum=0;
-    for(int i=0;i<numbers.length;i++)
-    {
-        sum=sum+numbers[i];
-    }
-    return sum;
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while (t-- > 0) {
-            int n = scn.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = scn.nextInt();
-            }
-            System.out.println(arraySum(arr));
-            System.out.println("~");
+class Solution {
+    public static int arraySum(int[] arr) {
+        // code here
+        int sum=0;
+        for(int i=0; i<arr.length; i++){
+            sum+= arr[i];
         }
-        scn.close();
+       return sum;
+        
     }
 }
-// } Driver Code Ends
