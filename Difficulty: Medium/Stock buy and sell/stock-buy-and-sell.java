7f7acd64f30e44422863c7600a38aed35d27a49b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-07 18:52:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.java
@@ -1,15 +1,28 @@
 
 
-// User function Template for Java
+// // User function Template for Java
 
-class Solution {
-    // Function to find the days of buying and selling stock for max profit.
-    int stockBuySell(int arr[]) {
-        int profit=0;
+// class Solution {
+//     // Function to find the days of buying and selling stock for max profit.
+//     int stockBuySell(int arr[]) {
+//         int profit=0;
+//         int n=arr.length;
+//         for(int i=0; i<n-1; i++){
+//             if(arr[i+1]>arr[i]){
+//                 profit+=arr[i+1]-arr[i];
+//             }
+//         }
+//         return profit;
+//     }
+// }
+
+class Solution  {
+    int stockBuySell(int arr[]){
+        int profit =0;
         int n=arr.length;
         for(int i=0; i<n-1; i++){
             if(arr[i+1]>arr[i]){
-                profit+=arr[i+1]-arr[i];
+                profit =profit+arr[i+1]-arr[i];
             }
         }
         return profit;
