c990b0905d5b5aab0da0721cae16123f7e503ca9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-07 18:47:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -1,37 +1,36 @@
-
 class Solution {
-   public void sort012(int[] arr) {
-    int left=0;
-    int right=arr.length-1;
-    int mid=0;
-    while (mid <= right) {
-        if (arr[mid] == 0) {
-            // Swap arr[left] and arr[mid]
-            int temp = arr[left];
-            arr[left] = arr[mid];
-            arr[mid] = temp;
-            left++;
-            mid++;
-        } else if (arr[mid] == 1) {
-            // Move mid forward
-            mid++;
-        } else if (arr[mid] == 2) {
-            // Swap arr[mid] and arr[right]
-            int temp = arr[mid];
-            arr[mid] = arr[right];
-            arr[right] = temp;
-            right--;
-        }
+    public void sort012(int[] arr) {
+        // code here
+       int count0=0;
+       int count1=0;
+       int count2=0;
+       for(int i=0;i<arr.length;i++){
+           if(arr[i]==0){
+               count0++;
+               
+           }
+           else if(arr[i]==1){
+               count1++;
+           }
+           else{
+               count2++;
+           }
+       }
+       int k=0;
+       while(count0>0){
+           arr[k]=0;
+           k++;
+           count0--;
+       }
+       while(count1>0){
+           arr[k]=1;
+           k++;
+           count1--;
+       }
+       while(count2>0){
+           arr[k]=2;
+           k++;
+           count2--;
+       }
     }
 }
-
-
-}
-
-
-        
-    
-
-
-
-
