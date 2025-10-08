7e5e0a451c60aaec9e367f063c735c3341bd09b6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-08 21:26:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.java
+++ Difficulty: Basic/Reverse a String/reverse-a-string.java
@@ -1,31 +1,13 @@
-
-
-
-
-
-
-// User function Template for Java
 class Solution {
-    public static String reverseString(String s) {
-        // String को character array में convert किया
-        //char[] chars = s.toCharArray();
-       
-
-       
-        char [] chars=s.toCharArray();
-        int left =0;
-        int right =chars.length-1;
-
-        // दो pointers से characters को swap करते हुए reverse किया
-        while (left < right) {
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
-            right--;
+    static String revStr(String s) {
+        // code here
+        
+        StringBuilder revStr = new StringBuilder();
+        
+        for(int i = s.length() -1 ; i >= 0; i--){
+            revStr.append(s.charAt(i));
         }
-
-        // final reversed string को return किया
-        return new String(chars);
+        
+        return revStr.toString();
     }
 }
