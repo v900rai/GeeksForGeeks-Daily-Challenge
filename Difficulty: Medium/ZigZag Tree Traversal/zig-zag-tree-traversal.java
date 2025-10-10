48d71df025a7cdffe519c99704e870e105260e51
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-10 10:45:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -1,113 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.util.*;
-import java.util.HashMap;
-import java.io.*;
-
-    class Node
-    {
-        int data;
-        Node left,right;
-        Node(int data){
-        this.data = data;
-        left=null;
-        right=null;
-    }
-    }
-		
-public class GFG2
-{
-    static Node buildTree(String str){
-        
-        if(str.length()==0 || str.charAt(0)=='N'){
-            return null;
-        }
-        
-        String ip[] = str.split(" ");
-        // Create the root of the tree
-        Node root = new Node(Integer.parseInt(ip[0]));
-        // Push the root to the queue
-        
-        Queue<Node> queue = new LinkedList<>(); 
-        
-        queue.add(root);
-        // Starting from the second element
-        
-        int i = 1;
-        while(queue.size()>0 && i < ip.length) {
-            
-            // Get and remove the front of the queue
-            Node currNode = queue.peek();
-            queue.remove();
-                
-            // Get the current node's value from the string
-            String currVal = ip[i];
-                
-            // If the left child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the left child for the current node
-                currNode.left = new Node(Integer.parseInt(currVal));
-                // Push it to the queue
-                queue.add(currNode.left);
-            }
-                
-            // For the right child
-            i++;
-            if(i >= ip.length)
-                break;
-                
-            currVal = ip[i];
-                
-            // If the right child is not null
-            if(!currVal.equals("N")) {
-                    
-                // Create the right child for the current node
-                currNode.right = new Node(Integer.parseInt(currVal));
-                    
-                // Push it to the queue
-                queue.add(currNode.right);
-            }
-            i++;
-        }
-        
-        return root;
-    }
-    
-	public static void inorder(Node root)
-	{
-	    if(root==null)
-	    return;
-	    inorder(root.left);
-	    System.out.print(root.data);
-	    inorder(root.right);
-	}
-     /* Drier program to test above functions */
-    public static void main(String args[]) throws IOException
-    {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	        
-	        int t=Integer.parseInt(br.readLine());
-    
-	        while(t > 0){
-	            String s = br.readLine();
-    	    	Node root = buildTree(s);
-        	    GFG g = new GFG();
-			
-			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
-			    for (int i = 0; i < res.size (); i++)
-			        System.out.print (res.get (i) + " ");
-			    System. out. println();  
-    			
-                t--;
-            
-        
-System.out.println("~");
-}
-	}
-}
-// } Driver Code Ends
 
 
 //User function Template for Java
@@ -123,34 +13,39 @@ System.out.println("~");
     }
 }*/
 
-class GFG {
-    // Function to store the zig zag order traversal of tree in a list.
+
+class Solution {
     ArrayList<Integer> zigZagTraversal(Node root) {
-        ArrayList<Integer> result = new ArrayList<>();
-        if (root == null) return result;
-
-        Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        int level=1;
-
-        while (!q.isEmpty()) {
-            int size = q.size();
-            LinkedList<Integer> levelList = new LinkedList<>(); // Use LinkedList for efficient addFirst and addLast
-
-            for (int i = 0; i < size; i++) {
-                Node temp = q.poll();
-                if (level%2==0) {
-                    levelList.addFirst(temp.data);
-                } else {
-                    levelList.addLast(temp.data);
-                }
-                if (temp.left != null) q.offer(temp.left);
-                if (temp.right != null) q.offer(temp.right);
-            }
-            result.addAll(levelList);
-            level++;
-        }
-        return result;
+        ArrayList<Integer> res = new ArrayList<>();
+         Queue<Node> q = new LinkedList<>();
+         q.offer(root);
+         int lvl = 0;
+         
+         while(!q.isEmpty()){
+             int size = q.size();
+             ArrayList<Integer> list = new ArrayList<>();
+             for(int i=0; i<size; i++){
+                 Node curr = q.poll();
+                 list.add(curr.data);
+                 
+                 if(curr.left != null){
+                     q.offer(curr.left);
+                 }
+                 
+                 if(curr.right != null){
+                     q.offer(curr.right);
+                 }
+             }
+             if(lvl%2 == 0){
+                 res.addAll(list);
+             }else{
+                 Collections.reverse(list);
+                 res.addAll(list);
+             }
+             lvl++;
+             
+         }
+         return res;
     }
 }
 
