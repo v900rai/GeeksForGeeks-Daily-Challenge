67566fb498c90d8d98f9b626ecff11762e962f24
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-10 19:33:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
+++ Difficulty: Easy/Search in a Matrix/search-in-a-matrix.java
@@ -1,55 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
-        while (t > 0) {
-            int rows = sc.nextInt();
-            int columns = sc.nextInt();
-
-            int matrix[][] = new int[rows][columns];
-
-            for (int i = 0; i < rows; i++) {
-                for (int j = 0; j < columns; j++) {
-                    matrix[i][j] = sc.nextInt();
-                }
-            }
-            int target = sc.nextInt();
-
-            Solution x = new Solution();
-
-            if (x.searchMatrix(matrix, target))
-                System.out.println("true");
-            else
-                System.out.println("false");
-            System.out.println("~");
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
-
-
 // User function Template for Java
 
 class Solution {
     // Function to search a given integer in a matrix.
     public boolean searchMatrix(int[][] mat, int x) {
-        int nums=mat.length;
-        boolean ans=false;
-        for(int i=0; i<nums; i++){
-            for(int j=0; j<mat[i].length; j++){
-            if(mat[i][j]==x){
-                ans=true;
-                break;
+        // code here
+        for(int num[] : mat){
+            for(int a:num){
+                if(a== x){
+                    return true;
+                }
             }
         }
+        return false;
     }
-    return ans;
-}
 }
