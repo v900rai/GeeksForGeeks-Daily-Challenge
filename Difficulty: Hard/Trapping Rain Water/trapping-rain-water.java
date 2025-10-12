bcbae2e90cdfd5e441f476cdc56821b443ce5483
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-12 11:53:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.java
@@ -8,11 +8,10 @@ class Solution {
         int left[] = new int[n];
         int rigth[] = new int [n];
         
-        // left m dal zero
         
-         left[0]=arr[0];
+        left[0] = arr[0];
         for(int i=1; i<n; i++){
-            left[i]=Math.max(left[i-1],arr[i]);
+            left[i] =Math.max(left[i-1],arr[i]);
         }
         rigth[n-1]=arr[n-1];
         for(int i=n-2; i>=0; i--){
