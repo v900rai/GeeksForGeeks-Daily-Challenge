ae30a179cda394eec13b38f12e1a9dc69d565eb6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-16 21:42:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Frequency in a Linked List/frequency-in-a-linked-list.java
@@ -0,0 +1,26 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+// complete the below function
+class Solution {
+    public static int count(Node head, int key) {
+        Node temp = head;
+        int count = 0;
+        while(temp!=null){
+            if(temp.data == key){
+                count++;
+            }
+            temp =temp.next;
+        }
+        return count;
+        
+    }
+}
