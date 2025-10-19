81c2ea315002e428fd1136e12f6cc7def7cc0d59
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-19 16:08:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Multiply two strings/multiply-two-strings.java
+++ Difficulty: Medium/Multiply two strings/multiply-two-strings.java
@@ -1,40 +1,15 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.math.*;
-import java.util.*;
-
-class Multiply {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t-- > 0) {
-            String a = sc.next();
-            String b = sc.next();
-            Solution g = new Solution();
-            System.out.println(g.multiplyStrings(a, b));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 
 
 //User function Template for Java
 
-class Solution
-{
-    public String multiplyStrings(String s1,String s2)
-    {
-        String product = "";
-        BigInteger a = new BigInteger(s1);
-        BigInteger b = new BigInteger(s2);
-        BigInteger mul = a.multiply(b);
-        product = product + mul.toString();
-        return product;
-        
+import java.math.BigInteger;
+
+class Solution {
+    public String multiplyStrings(String s1, String s2) {
+        BigInteger b1 = new BigInteger(s1);
+        BigInteger b2 = new BigInteger(s2);
+        return b1.multiply(b2).toString();
     }
 }
