c2e374e623e82bd4b034a4bb96aeec7605d489c8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-22 22:15:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.java
@@ -1,25 +1,37 @@
-class Solution
-{
-    public ArrayList<String> findPermutation(String s)
-    {
-        // Code here
-        Set<String> ans=new HashSet<>();
-        genPer(s, "",ans);
-        return new ArrayList(ans);
+class Solution {
+    
+    public ArrayList<String> findPermutation(String s) {
+        ArrayList<String> ans=new ArrayList<>();
+        char arr[]=s.toCharArray();
+        int index=0;
+        solve(arr,index,ans);
+        Collections.sort(ans);
+        return ans;
     }
-    public void genPer(String s, String a, Set<String> result)
-    {
-        if(s.isEmpty())
-        {
-            result.add(a);
+    
+    public void solve(char arr[],int index,ArrayList<String> ans){
+        
+        // base class
+        if(index>=arr.length){
+            ans.add(new String(arr));
             return;
         }
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            String nq=s.substring(0,i)+s.substring(i+1);
-            genPer(nq,a+ch,result);
+        
+        HashSet<Character> used=new HashSet<>();
+        for(int j=index;j<arr.length;j++){
+            if(used.contains(arr[j])) continue;
+            used.add(arr[j]);
+            swap(arr,index,j);
+            solve(arr,index+1,ans);
+            
+            // backtrack
+            swap(arr,index,j);
         }
     }
+    
+    public void swap(char arr[],int i,int j){
+        char temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
 }
-
