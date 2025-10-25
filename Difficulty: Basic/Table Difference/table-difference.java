2f161088cc489f724a986868a800912fdd143e67
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-25 13:07:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Table Difference/table-difference.java
+++ Difficulty: Basic/Table Difference/table-difference.java
@@ -1,14 +1,8 @@
-import java.util.*;
-
-public class Solution {
-    public static void main(String args[]) {
-        // Your Code Here
-        Scanner s =new Scanner(System.in);
-        int n1 = s.nextInt();
-        int n2 = s.nextInt();
-        int diff=n1-n2;
+class Solution {
+    public static void difference(int n1, int n2) {
+        // Write your code here
         for(int i=1;i<=10;i++){
-            System.out.print(diff*i+" ");
+            System.out.print((n1-n2)*i + " ");
         }
     }
 }
