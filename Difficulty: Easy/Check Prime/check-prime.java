7ba3dfa1858bb709928b87304b35bdfda23d0f41
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-25 12:46:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.java
+++ Difficulty: Easy/Check Prime/check-prime.java
@@ -1,16 +1,24 @@
-class Solution {
-    public static boolean prime(int n) {
-        // Write your code here
-          if (n == 2) {
-            return true;
-        } else if (n == 1) {
-            return false;
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+    
+         if (n < 2) {
+            System.out.println("False");
+            return;
         }
-        for(int i = 2; i <= n/2; i++) {
-            if(n % i == 0) {
-                return false;
+
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
             }
         }
-            return true; 
+
+        System.out.println(isPrime ? "True" : "False");
     }
 }
