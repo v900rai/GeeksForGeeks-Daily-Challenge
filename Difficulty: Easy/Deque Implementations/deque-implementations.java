aa1e58268932dac66857b9aab3eaa4fa12922b65
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-25 12:16:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Deque Implementations/deque-implementations.java
@@ -0,0 +1,30 @@
+class Solution {
+    public static void pb(ArrayDeque<Integer> dq, int x) {
+        //  code here
+        dq.offerLast(x);
+    }
+
+    public static void ppb(ArrayDeque<Integer> dq) {
+        
+         if(!dq.isEmpty()) 
+            dq.removeLast();
+       
+        //  code here
+    }
+
+        
+    public static int front_dq(ArrayDeque<Integer> dq) {
+        //  code here
+         if(dq.isEmpty()) 
+            return -1;
+    
+         return dq.peek();
+    }
+        
+
+    public static void pf(ArrayDeque<Integer> dq, int x) {
+        //  code here
+         dq.offerFirst(x);
+    }
+}
+
