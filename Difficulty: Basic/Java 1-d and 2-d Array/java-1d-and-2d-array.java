12c00afc21676b1f239d89f980a22991e5aa223a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-25 13:18:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Java 1-d and 2-d Array/java-1d-and-2d-array.java
+++ Difficulty: Basic/Java 1-d and 2-d Array/java-1d-and-2d-array.java
@@ -1,69 +1,20 @@
-//{ Driver Code Starts
-// Java program to demonstrate working of Scanner in Java
-import java.util.*;
-import java.io.*;
-class Abc
-{
-    public static void main(String args[])
-    {
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-        while(t-->0)
-        {
-            int n=sc.nextInt();
-            int[] a[],b;
-            a= new int[n][n];
-            b= new int[n];
-            int sum=0;
-            for(int i=0;i<n;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    int p=sc.nextInt();
-                    a[i][j]=p;
-                }
-            }
-            int Max=0;
-            for(int i=0;i<n;i++)
-            {
-                int p=sc.nextInt();
-                b[i]=p;
-            }
-            Complete obj = new Complete();
-            ArrayList<Integer> ans;
-            ans = obj.array(a, b, n);
-            for(int i: ans)
-                System.out.print(i + " ");
-            System.out.println();
-        
-System.out.println("~");
-}
-    }
-}
-
-
-// } Driver Code Ends
-
-
-class Complete
-{
-    public static ArrayList<Integer> array(int a[][], int b[], int n)
-    {
+class Complete {
+    public static ArrayList<Integer> array(int a[][], int b[], int n) {
         // Complete the function
-        ArrayList<Integer> lst = new ArrayList<>();
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-            sum += a[i][i];
+       ArrayList<Integer>ans = new ArrayList<>();
+        int sum = 0;
+        int max = 0;
+       for(int i =0;i<n;i++){
+           sum += a[i][i];
+       }
+        for(int num : b){
+            max = Math.max(num,max);
         }
-        
-        int max = Arrays.stream(b).max().getAsInt();
-        
-        lst.add(sum);
-        lst.add(max);
-        
-        return lst;
-    }
-    
-}
+        ans.add(sum);
+        ans.add(max);
+        return ans;
 
+        
+        
+    }
+}
