daf3b1d9cedf7a2f8fa90f1008d257ac6899f9dd
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-28 23:03:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.java
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.java
@@ -10,36 +10,20 @@ class Node
 */
 
 class Solution {
-    // Function to remove duplicates from sorted linked list.
-    // Node removeDuplicates(Node head) {
-    //      if(head ==null || head.next==null ){
-    //          return head;
-    //      }
-    //      Node curr =head;
-    //      while(curr!=null && curr.next!=null){
-    //          if(curr.data==curr.next.data){
-    //              curr.next=curr.next.next;
-    //          }
-    //          else{
-    //              curr=curr.next;
-    //          }
-    //      }
-    //      return head;
-         
-    // }
-  Node removeDuplicates(Node head) {
-      if(head == null || head.next == null){
-          return head;
-      }
-      Node curr = head;
-      while(curr!= null && curr.next!=null){
-          if(curr.data == curr.next.data){
-              curr.next=curr.next.next;
-          }
-          else{
-              curr=curr.next;
-          }
-      }
-     return  head;
-  }
+    //Function to remove duplicates from sorted linked list.
+    Node removeDuplicates(Node head) {
+         if(head ==null || head.next==null ){
+             return head;
+         }
+         Node curr =head;
+         while(curr!=null && curr.next!=null){
+             if(curr.data==curr.next.data){
+                 curr.next=curr.next.next;
+             }
+             else{
+                 curr=curr.next;
+             }
+         }
+         return head;
+}
 }
