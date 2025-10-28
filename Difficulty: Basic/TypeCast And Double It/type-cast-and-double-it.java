d38100781ce93a950a5514c5ad5e8a98c572b1d8
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-28 22:33:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/TypeCast And Double It/type-cast-and-double-it.java
+++ Difficulty: Basic/TypeCast And Double It/type-cast-and-double-it.java
@@ -4,8 +4,7 @@ class GFG {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         String num = sc.nextLine();
-        // TypeCast to int double it and print
-            // TypeCast to int double it and print
+
        int b = Integer.parseInt(num )*2;
         System.out.println(b);
     }
