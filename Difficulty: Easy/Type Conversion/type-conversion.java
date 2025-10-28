208d3369fe74e2cfc9f3832d2d053495661aa7e2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-10-28 22:43:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Type Conversion/type-conversion.java
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -1,12 +1,7 @@
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        double d = sc.nextDouble();
-        int x = (int)d;
-        // Typecast and print
-        System.out.println(x);
+class Solution {
+    public static int typeCast(double d) {
+        // code here
+         // code here
+        return (int)d;
     }
 }
