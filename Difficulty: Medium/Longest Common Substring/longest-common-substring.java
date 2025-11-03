249c75224d09d02e9e766dde824ba99c86c1ef2c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-03 15:05:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.java
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.java
@@ -1,25 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String S1 = read.readLine().trim();
-            String S2 = read.readLine().trim();
-
-            Solution ob = new Solution();
-            System.out.println(ob.longestCommonSubstr(S1, S2));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 
