3c0640c4a2d7748bf10201171a903105e27e0805
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-04 21:31:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Traversal Reverse/array-traversal-reverse.java
+++ Difficulty: Easy/Array Traversal Reverse/array-traversal-reverse.java
@@ -1,43 +1,8 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.util.Scanner;
-
-class Main {
-
-    
-// } Driver Code Ends
-
-// User function Template for Java
-
-public static void arrayTraversalReverse(int[] numbers) {
-    // Write your code below
-      for(int i=numbers.length-1;i>=0;i--)
-  {
-      System.out.print(numbers[i]+" ");
-  }
-
-    // Don't add a new line as it is provided by the driver code
-}
-
-
-//{ Driver Code Starts.
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int t = scn.nextInt();
-        while (t-- > 0) {
-            int n = scn.nextInt();
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = scn.nextInt();
-            }
-            arrayTraversalReverse(arr);
-            System.out.println();
-
-            System.out.println("~");
-        }
-        scn.close();
+class Solution {
+    public static void arrayTraversalReverse(int[] arr) {
+        // Code here
+         for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+         }
     }
 }
-// } Driver Code Ends
