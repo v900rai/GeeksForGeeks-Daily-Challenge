52a019ae0c1e97dfb8b645ad078396f5b2794a6d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-05 17:33:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Get Minimum Squares/get-minimum-squares.java
+++ Difficulty: Medium/Get Minimum Squares/get-minimum-squares.java
@@ -1,16 +1,52 @@
-// User function Template for Java
-
 class Solution {
-    public int MinSquares(int n) {
+    public int minSquares(int n) {
         // Code here
-        int[] dp = new int[n+1];
+         int dp[] = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-         for(int i =1; i <= n; i++) {
-            dp[i] = i;
-            for(int j =1; j* j <= i; j++) {
-                dp[i] = Math.min(dp[i],  1 + dp[i - (j*j)]); 
+        // return memo(n, dp);
+        return tabulation(n, dp);
+    }
+    
+    //Recursion TC = O((root(n))^n )
+    int solve(int n) {
+        if (n == 0) return 0;
+
+        int minCount = Integer.MAX_VALUE;
+
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            minCount = Math.min(minCount, 1 + solve(n - square));
+        }
+
+        return minCount;
+    }
+    
+    
+    // Memoization TC = O(n * root(n))
+    int memo(int n, int dp[]) {
+        if (n == 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        int minCount = Integer.MAX_VALUE;
+
+        for (int i = 1; i * i <= n; i++) {
+            int square = i * i;
+            minCount = Math.min(minCount, 1 + memo(n - square, dp));
+        }
+
+        return dp[n]=minCount;
+    }
+    
+    
+    // tabulation TC = O(n * root(n))
+    int tabulation(int n, int dp[]){
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j*j <= i;j++){
+                if(dp[i - j*j] != Integer.MAX_VALUE)
+                dp[i] = Math.min(dp[i], 1+dp[i-j*j]);
             }
         }
         return dp[n];
     }
 }
+
