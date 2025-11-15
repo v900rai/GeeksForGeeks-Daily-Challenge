f11bb347f15183db77e12e4b5f0065406182aba0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-15 21:11:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Kth distance/kth-distance.java
+++ Difficulty: Easy/Kth distance/kth-distance.java
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 // User function Template for Java
 
 import java.util.HashSet;
@@ -14,54 +6,17 @@ class Solution {
     public boolean checkDuplicatesWithinK(int[] arr, int k) {
         HashSet<Integer> set = new HashSet<>();
         
-        for (int i = 0; i < arr.length; i++) {
-            if (set.contains(arr[i])) {
-                return true; // Duplicate found within range k
+        for(int i=0; i<arr.length; i++){
+            if(set.contains(arr[i])){
+                return true;
             }
-            
             set.add(arr[i]);
-            
-            // Maintain the set size to ensure only elements within range k are stored
-            if (i >= k) {
-                set.remove(arr[i - k]);
+            if(i>=k){
+                set.remove(arr[i-k]);
             }
         }
-        
-        return false; // No duplicates found within range k
+        return false;
+       
     }
 }
 
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-            int k = Integer.parseInt(br.readLine());
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-            Solution obj = new Solution();
-            boolean res = obj.checkDuplicatesWithinK(arr, k);
-            if (res)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
