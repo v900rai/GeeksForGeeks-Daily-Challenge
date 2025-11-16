7be6de610ed521b4f4cb7ecd18c5319e7a1abe27
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-11-16 18:38:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
+++ Difficulty: Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -1,9 +1,5 @@
-
-
-//User function Template for Java
-
-/*class Node
-{
+/*
+class Node {
     int data;
     Node left,right;
     Node(int d)
@@ -11,41 +7,40 @@
         data=d;
         left=right=null;
     }
-}*/
-
+}
+*/
 
 class Solution {
     ArrayList<Integer> zigZagTraversal(Node root) {
-        ArrayList<Integer> res = new ArrayList<>();
-         Queue<Node> q = new LinkedList<>();
-         q.offer(root);
-         int lvl = 0;
-         
-         while(!q.isEmpty()){
-             int size = q.size();
-             ArrayList<Integer> list = new ArrayList<>();
-             for(int i=0; i<size; i++){
-                 Node curr = q.poll();
-                 list.add(curr.data);
-                 
-                 if(curr.left != null){
-                     q.offer(curr.left);
-                 }
-                 
-                 if(curr.right != null){
-                     q.offer(curr.right);
-                 }
-             }
-             if(lvl%2 == 0){
-                 res.addAll(list);
-             }else{
-                 Collections.reverse(list);
-                 res.addAll(list);
-             }
-             lvl++;
-             
-         }
-         return res;
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        if (root == null) {
+            return ans;
+        }
+        
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        boolean leftToRight = true;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            Integer[] level = new Integer[size];
+            
+            for (int i = 0; i < size; i++) {
+                Node node = q.poll();
+                int index = leftToRight ? i : size - 1 - i;
+                level[index] = node.data;
+                
+                if (node.left != null)
+                    q.offer(node.left);
+                    
+                if (node.right != null)
+                    q.offer(node.right);
+            }
+            
+            leftToRight = !leftToRight;
+            ans.addAll(Arrays.asList(level));
+        }
+        
+        return ans;
     }
 }
-
