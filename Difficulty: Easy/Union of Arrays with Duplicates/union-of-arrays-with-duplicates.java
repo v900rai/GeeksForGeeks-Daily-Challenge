508c039e804c4075783e5184328d88603f11747a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-02 18:11:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -2,19 +2,14 @@ class Solution {
     public static ArrayList<Integer> findUnion(int[] a, int[] b) {
         // Using HashSet to remove duplicates
         HashSet<Integer> set = new HashSet<>();
-        
-        for (int num : a) {
-            set.add(num);
+        for(int nums : a){
+            set.add(nums);
         }
-        for (int num : b) {
-            set.add(num);
+        for(int nums : b){
+            set.add(nums);
         }
-        
-       ArrayList<Integer> result = new ArrayList<>(set);
-        
-     
-       Collections.sort(result);
-        
+        ArrayList<Integer> result = new ArrayList<>(set);
+        Collections.sort(result);
         return result;
     }
 }
