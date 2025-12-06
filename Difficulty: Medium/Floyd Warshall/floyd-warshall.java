8c42682d75dd93a09aba6af99e7fb8a131860522
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-06 15:08:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -1,36 +1,3 @@
-//{ Driver Code Starts
-// Initial template for JAVA
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            int[][] matrix = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                String[] s = br.readLine().trim().split(" ");
-                for (int j = 0; j < n; j++) matrix[i][j] = Integer.parseInt(s[j]);
-            }
-            Solution obj = new Solution();
-            obj.floydWarshall(matrix);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(matrix[i][j] + " ");
-                }
-                System.out.println();
-            }
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function template for JAVA
