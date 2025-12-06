7f4e4ef27744fe7f9b214e80c4f34448cb124883
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-06 14:49:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,28 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int V = sc.nextInt();
-            int E = sc.nextInt();
-            int[][] edges = new int[E][2];
-            for (int i = 0; i < E; i++) {
-                edges[i][0] = sc.nextInt();
-                edges[i][1] = sc.nextInt();
-            }
-
-            Solution obj = new Solution();
-            boolean ans = obj.isCyclic(V, edges);
-            System.out.println(ans ? "true" : "false");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
 
 
 class Solution {
