5c925734f7d455c51a4b2de4664151d9391793e1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-09 11:16:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -5,13 +5,15 @@
 class Solution {
     static char nonRepeatingChar(String S) {
         // Character की frequency count करने के लिए HashMap
-        Map<Character, Integer> freqMap = new HashMap<>();
-
         
-        for(int i=0; i<S.length(); i++){
-            char ch=S.charAt(i);
-            freqMap.put(ch, freqMap.getOrDefault(ch,0)+1);
-        }
+        
+           Map<Character, Integer>  freqMap= new HashMap<>();
+           for(int i=0; i<S.length(); i++){
+               char ch=S.charAt(i);
+               freqMap.put(ch, freqMap.getOrDefault(ch,0)+1);
+           }
+
+
 
         // Step 2: फिर से traverse करके पहला non-repeating character ढूंढो
         for (int i = 0; i < S.length(); i++) {
