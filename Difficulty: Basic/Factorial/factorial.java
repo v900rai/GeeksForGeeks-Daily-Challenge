2365cf050d768f566226d8c9d9c619fb4cdcf94a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-11 11:15:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,12 +1,14 @@
 
-// User function Template for Java
-class Solution {
-    public static int nFactorial(int n) {
-        int ans = 1;
-        for(int i=1;i<=n;i++){
-            ans*=i;
-        }
 
-        return ans;
+class Solution {
+    static int factorial(int n) {
+        if(n==0 && n==1){
+            return 1;
+        }
+        int fact=1;
+        for(int i=2; i<=n; i++){
+            fact=fact*i;
+        }
+        return fact;
     }
 }
