a7d34d1c40d998719855110b52464b1fd0b90a81
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-16 10:57:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -1,38 +1,16 @@
-
-
-
 class Solution {
-    // Function to check if two strings are rotations of each other or not.
-    public static boolean areRotations(String s1, String s2) {
-              StringBuilder obj = new StringBuilder(s2);
-        int m = s2.length();
-        obj.append('$');
-        obj.append(s1);
-        obj.append(s1);
-        s1 = new String(obj);
-        int n = s1.length();
-        int lps[] = new int[n];
-        int len = 0;
-        int i =1;
-        while(i<n){
-            if(s1.charAt(i) ==s1.charAt(len)){
-                len++;
-                if(len==m){
-                    return true;
-                }
-                lps[i] =len;
-                i++;
+    public boolean areRotations(String s1, String s2) {
+        // code here
+        s1 = s1 + s1;
+        int i = 0;
+        int j = 0;
+        while(i < s1.length() && j < s2.length()){
+            if(s1.charAt(i) == s2.charAt(j)){
+                j++;
             }
-            else{
-                if(len>0){
-                    len =lps[len-1];
-                }
-                else{
-                    lps[i] = 0;
-                    i++;
-                }
-            }
+            i++;
         }
+        if(j == s2.length()) return true;
         return false;
     }
 }
