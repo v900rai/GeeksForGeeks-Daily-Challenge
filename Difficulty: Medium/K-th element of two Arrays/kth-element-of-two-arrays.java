929d4748fff6af66e5096378f15ccbdbc396ccc5
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-25 11:51:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -1,37 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int k = Integer.parseInt(br.readLine().trim());
-
-            String[] line1 = br.readLine().trim().split(" ");
-            int[] a = new int[line1.length];
-            for (int i = 0; i < line1.length; i++) {
-                a[i] = Integer.parseInt(line1[i]);
-            }
-
-            String[] line2 = br.readLine().trim().split(" ");
-            int[] b = new int[line2.length];
-            for (int i = 0; i < line2.length; i++) {
-                b[i] = Integer.parseInt(line2[i]);
-            }
-
-            Solution ob = new Solution();
-            System.out.println(ob.kthElement(a, b, k));
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
