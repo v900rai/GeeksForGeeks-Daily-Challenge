57da82fb37f6e602617eb1ddaa6b5a1967ea63d6
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-25 12:33:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search an element in sorted and rotated array/search-an-element-in-sorted-and-rotated-array.java
+++ Difficulty: Easy/Search an element in sorted and rotated array/search-an-element-in-sorted-and-rotated-array.java
@@ -1,13 +1,31 @@
 // User function Template for Java
 
 class Solution {
-    static int Search(int arr[], int target) {
-        for(int i=0; i<arr.length; i++){
-            if(target == arr[i]){
-                return i;
+    static int Search(int ar[], int target) {
+        int pointOfRot = findPointOfRot(ar, 0, ar.length-1, ar[ar.length-1]);
+
+        int ans = -1;
+        if (pointOfRot > 0 && target >= ar[0]) {
+            ans = Arrays.binarySearch(ar, 0, pointOfRot, target);
+        } else {
+            ans = Arrays.binarySearch(ar, pointOfRot, ar.length, target);
+        }
+        return ans < 0 ? -1 : ans;
+    }
+    
+    static int findPointOfRot(int ar[], int l, int r, int last) {
+        int m = l + (r-l)/2;
+        if (m-1 >= 0) {
+            if (ar[m-1] > ar[m])
+                return m;
+            else {
+                if (ar[m] > last) {
+                    return findPointOfRot(ar, m+1, r, ar[r]);
+                } else {
+                    return findPointOfRot(ar, l, m-1, ar[m-1]);
+                }
             }
         }
-        return -1;
-        
+        return 0;
     }
 }
