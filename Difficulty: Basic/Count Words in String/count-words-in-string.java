32543e211ef5f9134dc521c812a90551ed52111f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-27 11:22:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Words in String/count-words-in-string.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    // Complete the function
+    // str: input string
+    public static int countWords(String str) {
+        // find and return the number of words
+        // present in the string
+        
+        return str.split(" ").length;
+    }
+}
