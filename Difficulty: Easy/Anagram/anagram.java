ce401feb1c615bf0ece55ca2eb4602f9efcd7c2d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2025-12-30 17:56:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -1,6 +1,8 @@
 class Solution {
     public static boolean areAnagrams(String s1, String s2) {
-       
+       if(s1.length()!=s2.length()){
+           return false;
+       }
         
         char c1[] = s1.toCharArray();
         char c2[] = s2.toCharArray();
