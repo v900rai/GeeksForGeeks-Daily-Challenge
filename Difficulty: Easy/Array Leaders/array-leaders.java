bab03cdb7ef7cd54bce5e912feaf1dfdac0c375f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-04 13:23:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -5,12 +5,13 @@ class Solution {
         
         int currSum= arr[arr.length-1];
         
+     
+        
         for(int i= arr.length-1; i>=0; i--){
             if(currSum <=arr[i]){
                 list.add(0,arr[i]);
-                currSum = arr[i];
+                currSum =arr[i];
             }
-            
         }
         return list;
     }
