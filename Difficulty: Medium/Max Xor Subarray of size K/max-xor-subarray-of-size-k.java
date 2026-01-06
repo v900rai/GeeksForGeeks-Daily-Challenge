972ef973eaec5260baf5e5bd71c39bd6f678dfac
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-06 11:09:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Xor Subarray of size K/max-xor-subarray-of-size-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxSubarrayXOR(int[] arr, int k) {
+        // code here
+        if(k> arr.length || k==0){
+            return 0;
+        }
+        int i=0;
+        int j=0;
+        int xor = 0;
+        int max =0;
+        while(j<arr.length){
+            xor = xor ^ arr[j];
+            if(j-i+1 >k){
+                xor ^= arr[i];
+                i++;
+            }
+             if(j-i+1==k){
+                max = Math.max(max,xor);
+            }
+            j++;
+        }
+        return  max;
+    }
+}
