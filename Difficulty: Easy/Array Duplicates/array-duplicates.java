5d844474c4fb010e5cfd1b0c0a4b8ff814b41266
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-08 21:31:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Duplicates/array-duplicates.java
+++ Difficulty: Easy/Array Duplicates/array-duplicates.java
@@ -1,16 +1,16 @@
 class Solution {
     public ArrayList<Integer> findDuplicates(int[] arr) {
-     
-       HashSet <Integer> set = new HashSet<>();
-       ArrayList<Integer> list = new ArrayList<>();
-       for(int num : arr){
-           if(set.contains(num)){
-               list.add(num);
-           }
-           else{
-               set.add(num);
-           }
-       }
-       return list;
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int nums :arr){
+            if(set.contains(nums)){
+                list.add(nums);
+            }
+            else{
+                set.add(nums);
+            }
+        }
+        return list;
+        
     }
 }
