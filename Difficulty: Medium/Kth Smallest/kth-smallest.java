97315604ad1691a262473171e50c9ac7c8cb1394
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-17 01:47:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -1,18 +1,13 @@
-
-
-// User function Template for Java
-
 class Solution {
-    public static int kthSmallest(int[] arr, int k) {
-     
-     Arrays.sort(arr);
-     int count=0;
-     for(int i=0; i<k; i++){
-         count=arr[i];
-         
-     }
-     return count;
-    
+    public int kthSmallest(int[] arr, int k) {
+           
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i:arr){
+            pq.add(i);
+            if(pq.size() > k)
+                pq.poll();
+        }
+        return pq.peek();
     }
-    
 }
