1bcb646666d26b5adcfe98e3c7ad639c9c85dfda
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-17 01:20:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -3,7 +3,7 @@ class Solution {
        int sum=0;
        int max=Integer.MIN_VALUE;
        for(int i=0; i<arr.length; i++){
-           sum+=arr[i];
+           sum= sum+arr[i];
            max=Math.max(sum ,max);
            if(sum<0){
                sum=0;
