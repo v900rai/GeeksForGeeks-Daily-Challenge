6f9c64357cea22d5a180b1cfb3fc01925d5fceba
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-20 11:35:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.java
+++ Difficulty: Medium/Sort a stack/sort-a-stack.java
@@ -1,27 +1,16 @@
-/*Complete the function below*/
-class GfG {
-    public Stack<Integer> sort(Stack<Integer> s) {
-        // add code here.
-        
-        Stack<Integer> tem=new Stack<>();
-        int len=s.size();
-        int arr[]=new int[len];
-        int i=0;
-        while(!s.isEmpty()){
-            arr[i++]=s.pop();
-            
-        }
-        
-        Arrays.sort(arr);
-        
-        for(int j=0;j<len;j++){
-            tem.push(arr[j]);
-        }
-        
-        return tem;
-        
-            
-            
+class Solution {
+    public void sortStack(Stack<Integer> st) {
+       ArrayList<Integer> a =new ArrayList<>();
+       
+       while(!st.empty())
+       {
+           a.add(st.pop());
+       }
+       Collections.sort(a);
+       for(int x:a)
+       {
+           st.push(x);
+       }
         
     }
 }
