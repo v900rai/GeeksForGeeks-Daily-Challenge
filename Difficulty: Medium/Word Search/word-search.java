7f494601e4638400c55c2f17be3f5f6be4f60fc3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-27 10:18:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.java
+++ Difficulty: Medium/Word Search/word-search.java
@@ -1,73 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt(); // Number of test cases
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] mat = new char[n][m];
-
-            // Reading the matrix
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    mat[i][j] = sc.next().charAt(0);
-                }
-            }
-
-            String word = sc.next();
-            Solution obj = new Solution();
-            boolean ans = obj.isWordExist(mat, word);
-            if (ans)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
-    static public boolean isWordExist(char[][] mat, String word) {
-        // Code here
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                boolean[][] visit = new boolean[mat.length][mat[0].length];
-                if(check(mat, i, j, 0, word, visit)){
-                    return true;
-                }
+    int[][] vis;
+    int[] dx = {-1,0,1,0};
+    int[] dy = {0,1,0,-1}; 
+    private boolean check(int x,int y,int w,int n,int m,char[][] mat, String word){
+        if(w==word.length())return true;
+        vis[x][y]=1;
+        for(int d=0;d<4;d++){
+            int nx = x+dx[d];
+            int ny = y+dy[d];
+            if(nx>=0 && ny>=0 && nx<n && ny<m && vis[nx][ny]==0){
+                if(mat[nx][ny]==word.charAt(w) && check(nx,ny,w+1,n,m,mat,word))return true;
+            }
+        }
+        vis[x][y]=0;
+        return false;
+    }
+    public boolean isWordExist(char[][] mat, String word) {
+        int n = mat.length,m=mat[0].length;
+        vis = new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==word.charAt(0) && check(i,j,1,n,m,mat,word))return true;
             }
         }
         return false;
-    }
-    
-    static boolean check(char[][] mat, int i, int j, int idx, String word, boolean[][] visit){
-        if(idx == word.length()){
-            return true;
-        }
-        
-        if(i < 0 || i >= mat.length || j < 0 || j >= mat[0].length || visit[i][j] 
-        || mat[i][j] != word.charAt(idx)){
-            return false;
-        }
-        
-        visit[i][j] = true;
-        if(check(mat, i, j-1, idx+1, word, visit) ||
-           check(mat, i, j+1, idx+1, word, visit) ||
-           check(mat, i-1, j, idx+1, word, visit) ||
-           check(mat, i+1, j, idx+1, word, visit) ){
-               return true;
-           }
-           
-           visit[i][j] = false;
-           return false;
-           
     }
 }
