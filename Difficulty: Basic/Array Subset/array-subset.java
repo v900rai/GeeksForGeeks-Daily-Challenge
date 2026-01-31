e2b864a011855d34229413faec5f7bca99be0eaf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-31 12:06:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Subset/array-subset.java
+++ Difficulty: Basic/Array Subset/array-subset.java
@@ -1,27 +1,26 @@
 
 class Solution {
     public boolean isSubset(int a[], int b[]) {
+        
        Arrays.sort(a);
        Arrays.sort(b);
-        int i=0;
-        int j=0;
-        int count = 0;
-        while(i<a.length && j<b.length){
-            if(a[i] == b[j]){
-            count++;
-            i++;
-            j++;
-            }
-            else{
-            i++;
-            }
-            
-            
-        }
-        if(count == b.length){
-            return true;
-        }
-        return false;
-        
+       int count = 0;
+       
+       int  i =0;
+       int j = 0;
+       while(i<a.length && j<b.length){
+           if(a[i] == b[j]){
+               count++;
+               i++;
+               j++;
+           }
+           else{
+           i++;
+       }
+       }
+       if(count == b.length){
+           return true;
+       }
+       return false;
     }
 }
