8a6a4f1d84f31d757499a1269e15e59863252565
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-01-31 11:53:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -1,17 +1,17 @@
 
 class Solution {
     static ArrayList<Integer> subarraySum(int[] arr, int target) {
-        // code here
+        
         ArrayList <Integer>  list = new ArrayList<>();
         int n = arr.length;
-        int sum =0;
+        int sum = 0;
         
-        int i=0;
-        for(int j=0; j<n; j++){
-            sum = sum+arr[j];
+        int i = 0;
+        for(int j = 0; j<n; j++){
+            sum = sum + arr[j];
             
-            while(sum > target && i<j){
-                sum = sum-arr[i];
+            while(sum > target && i < j){
+                sum = sum -arr[i];
                 i++;
             }
             if(sum == target){
