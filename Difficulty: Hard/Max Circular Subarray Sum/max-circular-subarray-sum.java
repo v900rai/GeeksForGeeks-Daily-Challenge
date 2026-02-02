fec70037fb6b00a2e29b73298a56088136f76320
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 09:44:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.java
@@ -1,65 +1,28 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().circularSubarraySum(arr));
-            // System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-
-    // a: input array
-    // n: size of array
-    // Function to find maximum circular subarray sum.
-  
 class Solution {
-    // Function to find the maximum circular subarray sum.
-    public int circularSubarraySum(int arr[]) {
-        int n = arr.length;
-
-        int maxKadane = kadane(arr);
-
-        int totalSum = 0;
-        for (int i = 0; i < n; i++) {
-            totalSum += arr[i];
-            arr[i] = -arr[i];
+    public int maxCircularSum(int arr[]) {
+        // code here
+        int maxSoFar = arr[0];
+        int max = arr[0];
+        int minSoFar = arr[0];
+        int min = arr[0];
+        
+        int total = arr[0];
+        
+        for(int i=1;i<arr.length;i++)
+        {
+            max = Math.max(arr[i],arr[i]+max);
+            maxSoFar = Math.max(maxSoFar,max);
+            
+            min = Math.min(arr[i],arr[i]+min);
+            minSoFar = Math.min(minSoFar,min);
+            
+            total+=arr[i];
         }
-
-        int minSubarraySum = -kadane(arr);
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = -arr[i];
-        }
-
-        int maxCircular = totalSum - minSubarraySum;
-
-        if (maxCircular == 0) {
-            return maxKadane;
-        }
-
-        return Math.max(maxKadane, maxCircular);
-    }
-
-    private int kadane(int[] arr) {
-        int maxSum = Integer.MIN_VALUE;
-        int sum = 0;
-
-        for (int num : arr) {
-            sum = Math.max(num, sum + num);
-            maxSum = Math.max(maxSum, sum);
-        }
-
-        return maxSum;
+        
+        if(maxSoFar<0)
+        return maxSoFar;
+        
+        return Math.max(maxSoFar,total-minSoFar);
+        
     }
 }
