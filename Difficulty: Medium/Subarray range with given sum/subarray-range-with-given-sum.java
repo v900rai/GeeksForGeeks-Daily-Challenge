735a9bc3ff6e51cae1a940fbdda7ee29d6a35e09
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 15:16:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.java
+++ Difficulty: Medium/Subarray range with given sum/subarray-range-with-given-sum.java
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 // User function Template for Java
 
 class Solution {
@@ -33,37 +25,3 @@ class Solution {
  
 
 
-
-//{ Driver Code Starts.
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-
-            String line = br.readLine();
-            String[] tokens = line.split(" ");
-
-            // Create an ArrayList to store the integers
-            ArrayList<Integer> array = new ArrayList<>();
-
-            // Parse the tokens into integers and add to the array
-            for (String token : tokens) {
-                array.add(Integer.parseInt(token));
-            }
-
-            int[] arr = new int[array.size()];
-            int idx = 0;
-            for (int i : array) arr[idx++] = i;
-
-            int tar = Integer.parseInt(br.readLine());
-            Solution obj = new Solution();
-            int res = obj.subArraySum(arr, tar);
-
-            System.out.println(res);
-            // System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
