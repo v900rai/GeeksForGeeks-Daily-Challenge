da0ecd975e9ed9c8764ce05c8e2c1102d0a76f35
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 10:21:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/nextLine and next/next-line-and-next.java
@@ -0,0 +1,32 @@
+
+
+//User function Template for Java
+
+//Complete the function
+//Take input for a and s 
+
+
+
+
+class Geeks {
+static void getInput () {
+ 
+    Scanner sc = new Scanner(System.in);
+    int t=sc.nextInt(); //Taking the number of testcases
+    while(t-->0)
+    {
+    int a = sc.nextInt();
+    sc.nextLine(); //a will have int value along with it '/n' will be stored in buffer/memory.
+        String s =sc.nextLine(); //nextLine will read new line character so it will print a new line only.
+        //so we need to input the string once again.
+        
+        //Your code here
+    
+    System.out.println(a);
+    System.out.println(s);
+    }
+ 
+}
+}
+
+
