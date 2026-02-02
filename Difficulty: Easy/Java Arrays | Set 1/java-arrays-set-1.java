b997df15193daaede4aa40c9066bc5d416600057
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 10:13:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Java Arrays | Set 1/java-arrays-set-1.java
+++ Difficulty: Easy/Java Arrays | Set 1/java-arrays-set-1.java
@@ -1,12 +1,15 @@
+import java.util.*;
 class Solution {
     public String average(int arr[]) {
-        
-        long sum=0;
-        for(int i=0;i<arr.length;i++ ){
-            sum = sum + arr[i];
+        // code here
+        int m = arr.length;
+        int sum =0;
+        for(int i=0;i<m;i++){
+            sum = sum+arr[i];
+            
         }
-       double avg = (double)sum/arr.length;
-    return String.format("%.2f",avg);
+        double avg = (double)sum/arr.length;
+        return String.format("%.2f",avg);
         
     }
-}   
+}
