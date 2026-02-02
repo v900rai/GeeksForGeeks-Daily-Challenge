c3240b796cbd7851118d4fee405a9dad85e32063
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-02 09:52:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Java Hello World/java-hello-world.java
+++ Difficulty: Basic/Java Hello World/java-hello-world.java
@@ -1,6 +1,6 @@
 class Solution {
     static void printHelloWorld() {
-        // code here
-        System.out.println("Hello World");
+        System.out.println(" Hello World");
+        
     }
 }
