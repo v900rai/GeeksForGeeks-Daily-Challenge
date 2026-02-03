c739286e2125a12e46201660d6dfc1ea99beeeb3
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 18:16:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -6,8 +6,12 @@ class Solution {
         
         char c1[] = s1.toCharArray();
         char c2[] = s2.toCharArray();
+        
+        
         Arrays.sort(c1);
         Arrays.sort(c2);
+        
+        //return Arrays.equals(c1,c2);
         return Arrays.equals(c1,c2);
     }
 }
