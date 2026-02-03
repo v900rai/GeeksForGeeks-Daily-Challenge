298d4af02c8b9ab6686a0a19e2b78a486ad6d199
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 18:26:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.java
@@ -1,42 +1,27 @@
-//{ Driver Code Starts
-/*package whatever //do not write package name here */
 
-import java.io.*;
-import java.util.*;
 
-class GFG {
-	public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0){
-            Solution obj = new Solution();
-            System.out.println(obj.reverse(sc.next()));
-        
-System.out.println("~");
-}
-	}
-}
-
-// } Driver Code Ends
 
 
 class Solution {
     
-    public String reverse(String S){
-         //code here
-        Stack<Character> stack = new Stack<>();
-        StringBuffer sb = new StringBuffer();
-        for(int i = 0;i<S.length();i++)
-        {
-            stack.push(S.charAt(i));
-        }
-        while(stack.size() > 0)
-        {
-            sb.append(stack.pop());
+    public String reverse(String S) {
+        // Stack to store characters of the string
+        Stack<Character> s = new Stack<>();
+        
+        // StringBuilder to build the reversed string
+        StringBuilder ans = new StringBuilder();
+        
+        // Push each character of the string onto the stack
+        for (char c : S.toCharArray()) {
+            s.push(c);
         }
         
-        return sb.toString();
+        // Pop each character from the stack and append it to the result
+        while (!s.isEmpty()) {
+            ans.append(s.pop());
+        }
         
+        // Return the reversed string
+        return ans.toString();
     }
-
 }
