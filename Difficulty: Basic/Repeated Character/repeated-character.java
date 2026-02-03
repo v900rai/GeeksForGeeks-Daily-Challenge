b3cf12fa3e8372214e46301846790c20cec6511c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 18:31:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Repeated Character/repeated-character.java
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -1,33 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0)
-        {
-            String s;
-            s = sc.next();
-            
-            Solution ob = new Solution();
-            char res = ob.firstRep(s);
-            if (res == '#')
-                System.out.println(-1);
-            else
-                System.out.println(res);
-             
-        
-System.out.println("~");
-}
-    }
-}
-// } Driver Code Ends
 
 
 //User function Template for Java
