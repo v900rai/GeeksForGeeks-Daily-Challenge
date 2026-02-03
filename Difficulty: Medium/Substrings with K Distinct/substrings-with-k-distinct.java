ca5cdd2765d571ac009ea480f138352b0f15636c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 18:18:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
+++ Difficulty: Medium/Substrings with K Distinct/substrings-with-k-distinct.java
@@ -1,28 +1,20 @@
 class Solution {
-    
-    public int solve(String s , int k){
-        int i = 0;
-        int j = 0;
-        HashMap<Character , Integer> map = new HashMap<>();
-        int n = s.length();
-        int ans = 0;
-        
-        while(j < n){
-            map.put(s.charAt(j) , map.getOrDefault(s.charAt(j) , 0) + 1);
-            while(map.size() > k){
-                map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) - 1);
-                if(map.get(s.charAt(i)) == 0){
-                    map.remove(s.charAt(i));
-                }
-                i++;
-            }
-            ans += (j - i + 1);
-            j++;
-        }
-        return ans;
+    int countSubstr(String s, int k) {
+        return countAtMost(s,k) - countAtMost(s,k-1);
     }
-    public int countSubstr(String s, int k) {
-        //  code here
-        return solve(s , k) - solve(s , k - 1);
+    int countAtMost(String s, int k){
+        int l=0,count=0;
+        HashMap<Character,Integer>map = new HashMap<>();
+        for(int r=0; r<s.length(); r++){
+            char ch = s.charAt(r);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+            while(map.size() > k){
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                map.remove(s.charAt(l),0);
+                l++;
+            }
+            count += (r-l+1);
+        }
+        return count;
     }
 }
