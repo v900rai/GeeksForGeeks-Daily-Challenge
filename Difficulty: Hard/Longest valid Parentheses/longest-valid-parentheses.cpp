2aa7ba2af18ae94f05467cd3bc837bfb70655046
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 18:20:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
+++ Difficulty: Hard/Longest valid Parentheses/longest-valid-parentheses.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
@@ -34,22 +26,3 @@ class Solution {
     }
 };
 
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string S;
-        cin >> S;
-
-        Solution ob;
-        cout << ob.maxLength(S) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
