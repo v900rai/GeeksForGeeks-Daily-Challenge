3a98ed382aecd498e740fb056772ac0deccf535b
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-03 19:01:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
@@ -1,43 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String str = br.readLine().trim();
-
-            String ans = new Solution().removeDuplicates(str);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    String removeDuplicates(String s) 
-    {
-        Set<Character> st=new LinkedHashSet<>();
-        for(int i=0; i<s.length(); i++){
-            char ch=s.charAt(i);
-            st.add(ch);
+    String removeDuplicates(String s) {
+        HashSet<Character> set= new HashSet<> ();
+        String out="";
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(set.add(c))
+            {
+                out+=c;
+            }
         }
-        String rem = "";
-        for(Character ch : st) {
-            rem += ch;
-        }
-        return rem;
+        return out;
     }
 }
+
