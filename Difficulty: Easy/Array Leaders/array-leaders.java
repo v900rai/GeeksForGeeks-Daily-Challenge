99e84267c288dad181e4df55dd0ecfe3918c041f
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-04 12:35:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Array Leaders/array-leaders.java
+++ Difficulty: Easy/Array Leaders/array-leaders.java
@@ -2,11 +2,7 @@ class Solution {
     static ArrayList<Integer> leaders(int arr[]) {
     
         ArrayList<Integer> list = new ArrayList<>();
-        
         int currSum= arr[arr.length-1];
-        
-     
-        
         for(int i= arr.length-1; i>=0; i--){
             if(currSum <=arr[i]){
                 list.add(0,arr[i]);
