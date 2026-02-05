006105544a277bc625507fcba6d6000fd5ca06e1
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-05 11:01:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
+++ Difficulty: Medium/Maximize Number of 1's/maximize-number-of-1s.java
@@ -1,22 +1,26 @@
 class Solution {
- 
-
-public int maxOnes(int arr[], int k) {
+    public int maxOnes(int arr[], int k) {
+        //longest sub arrray with atmost k zeroes
+        //Two pointer sliding window
+        int n = arr.length;
         int left = 0;
-        int right = 0;
-        int count = 0;
-        
-        for(; right < arr.length; right++) {
-            if(arr[right] == 0) {
-                count++;
+        int zeroes = 0;
+        int max = 0;
+        for(int right = 0;right<n;right++){
+            if(arr[right] == 0){
+                zeroes++;
             }
-            if(count > k) {
-                if(arr[left] == 0)
-                count--;
+            while(zeroes > k){
+                if(arr[left] == 0){
+                    zeroes--;
+                }
                 left++;
             }
+            if(zeroes<=k){
+                int len = right-left+1;
+                max = Math.max(max,len);
+            }
         }
-        
-        return right - left;
+        return max;
     }
 }
