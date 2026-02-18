d15d8cc1872373645ed3ac3ba34e290b74755489
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-18 09:56:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -8,6 +8,7 @@ class Solution {
           Map<Character, Integer> freqMap = new HashMap<>();
           for(int i=0; i<S.length(); i++){
               char ch = S.charAt(i);
+             // freqMap.put(ch, freqMap.getOrDefault(ch, 0)+1);
               freqMap.put(ch, freqMap.getOrDefault(ch,0)+1);
           }
           
