9df87729d3d73b2da64e0b4c5e2b792c62313282
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-21 11:07:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find H-Index/find-hindex.java
+++ Difficulty: Medium/Find H-Index/find-hindex.java
@@ -1,24 +1,15 @@
 class Solution {
-    public int hIndex(int[] nums) {
-        // code here
-        int n = nums.length;
-        int max = nums[0];
-        for(int i=0;i<n;i++) max = Math.max(nums[i],max);
+    public int hIndex(int[] citations) {
+         Arrays.sort(citations);
+         int n= citations.length;
+         
+         for(int i=0; i<n; i++){
+             int h = n-i;
+             if(citations[i] >= h){
+                 return h;
+             }
+         }
+         return 0;
         
-        int [] pref = new int[max+1];
-        
-        for(int i=0;i<n;i++){
-            pref[nums[i]]++;
-        }
-        
-        for(int i=max-1;i>=0;i--){
-            pref[i]+=pref[i+1];
-        }
-        
-        for(int i=max;i>=0;i--){
-            if(pref[i]>=i) return i;
-        }
-        return 0;
     }
 }
- 
