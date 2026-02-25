40562e33b44b6e8e8b2f1f83bacd23e50b5246dc
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-25 11:36:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
+++ Difficulty: Medium/Longest Subarray with Majority Greater than K/longest-subarray-with-majority-greater-than-k.java
@@ -1,35 +1,19 @@
-
-// User function Template for Java
 class Solution {
-    static int longestSubarray(int[] arr, int k) {
-	   int[] change = new int[arr.length];
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]>k) {
-            	change[i]=1;
-            }
-            else {
-            	change[i]=-1;
-            }
+    public int longestSubarray(int[] arr, int k) {
+        // Code Here
+          int n = arr.length, count = 0, length = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for(int i = 0; i < n; i++){
+            if(arr[i] <= k) count--;
+            else count++;
+            
+            if(count > 0) length = Math.max(length, i + 1);
+            if(map.containsKey(count - 1)) length = Math.max(length, i - map.get(count - 1));
+            
+            if(!map.containsKey(count)) map.put(count, i);
         }
-        int prefixSum = 0;
-        HashMap<Integer, Integer> prefixMap = new HashMap<>();
-        prefixMap.put(0, -1);        
-        int maxLength = 0;
-
-        for (int i = 0; i < change.length; i++) {
-            prefixSum += change[i];
-            if (prefixSum > 0) {
-                maxLength = Math.max(maxLength, i + 1);
-            } else {
-                if (prefixMap.containsKey(prefixSum - 1)) {
-                    maxLength = Math.max(maxLength, i - prefixMap.get(prefixSum - 1));
-                }
-            }
-            if (!prefixMap.containsKey(prefixSum)) {
-                prefixMap.put(prefixSum, i);
-            }
-        }
-        return maxLength;
-	}
+        return length;
+    }
 }
 
