e61da6dac3ad1fd48ec0afa3a59fcbe593d4e82a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-26 19:47:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Non Repeating Character/non-repeating-character.java
+++ Difficulty: Easy/Non Repeating Character/non-repeating-character.java
@@ -13,9 +13,11 @@ class Solution {
           }
           
 
-        for(int i = 0; i<S.length(); i++){
+      
+        
+        for(int i=0; i<S.length(); i++){
             char ch = S.charAt(i);
-            if(freqMap.get(ch) == 1){
+            if(freqMap.get(ch)==1){
                 return ch;
             }
         }
