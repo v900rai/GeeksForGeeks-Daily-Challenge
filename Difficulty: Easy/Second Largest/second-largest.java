c74ed69304f4a4abddfc9b1c1a2cc733a84d30bb
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-28 00:00:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -16,9 +16,8 @@ class Solution {
              first =arr[i];
              
          }
-         
-         else if(arr[i]>second && first != arr[i]){
-             second =arr[i];
+         else  if(arr[i] >second && first != arr[i]){
+             second = arr[i];
          }
      }
      if(second==Integer.MIN_VALUE){
