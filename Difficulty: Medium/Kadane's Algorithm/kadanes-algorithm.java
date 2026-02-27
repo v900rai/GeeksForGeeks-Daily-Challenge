c5b284842860153df0e5854f8f2bd409167288cf
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-02-28 00:02:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -5,8 +5,9 @@ class Solution {
       for(int i=0; i<nums.length; i++){
           sum = sum + nums[i];
           
-          max = Math.max(max ,sum);
-          if(sum < 0){
+        
+          max  = Math.max(max , sum);
+          if(sum <0){
               sum = 0;
           }
       }
