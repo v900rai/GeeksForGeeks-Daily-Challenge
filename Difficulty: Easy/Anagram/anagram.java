9b0d47ffa5ec045d7aea4144a904b0117c733b49
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-03 21:08:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -3,13 +3,10 @@ class Solution {
        if(s1.length()!=s2.length()){
            return false;
        }
-        
     
-  
         char c1[] = s1.toCharArray();
         char c2[] = s2.toCharArray();
-        
-        
+    
         Arrays.sort(c1);
         Arrays.sort(c2);
         
