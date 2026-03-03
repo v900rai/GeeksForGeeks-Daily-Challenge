afcde7e2a046b8b7f4ba8df8f137cef226792d3c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-03 18:55:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Two Repeated Elements/two-repeated-elements.java
+++ Difficulty: Medium/Two Repeated Elements/two-repeated-elements.java
@@ -1,12 +1,14 @@
 class Solution {
     public int[] twoRepeated(int[] nums) {
        int[] arr = new int[2];
-       int j = 0;
+       int count =0;
        HashSet<Integer>set = new HashSet<>();
        for(int i = 0;i<nums.length;i++){
            if(set.contains(nums[i])){
-               arr[j++] = nums[i];
+               arr[count] = nums[i];
+               count++;
            }
+           
            else{
                set.add(nums[i]);
            }
