d222d1695d637e273a4db3a1da45168bdc25d7ed
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-15 17:11:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/1-D prefix sum/1d-prefix-sum.java
+++ Difficulty: Easy/1-D prefix sum/1d-prefix-sum.java
@@ -1,9 +1,5 @@
 class Solution {
     public ArrayList<Integer> prefSum(int[] arr) {
-        
-        
-        
-        
         ArrayList<Integer> result  = new ArrayList<>();
         int current = 0;
         for(int i=0; i<arr.length; i++){
