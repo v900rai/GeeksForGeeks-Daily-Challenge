550312fe97d2ddba112248a08470d6148a2db206
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-24 14:03:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil The Floor/ceil-the-floor.java
+++ Difficulty: Easy/Ceil The Floor/ceil-the-floor.java
@@ -1,30 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            int x = Integer.parseInt(br.readLine());
-            String[] input = br.readLine().split(" ");
-            int[] arr = new int[input.length];
-            for (int i = 0; i < input.length; i++) {
-                arr[i] = Integer.parseInt(input[i]);
-            }
-
-            Solution ob = new Solution();
-            int[] ans = ob.getFloorAndCeil(x, arr);
-            System.out.println(ans[0] + " " + ans[1]);
-        
-System.out.println("~");
-}
-    }
-}
-
-// } Driver Code Ends
 
 
 // User function Template for Java
@@ -35,7 +8,8 @@ class Solution {
         int n = arr.length;
 
 // LOWER BOUND
-        int low=0,high=n-1;
+        int low=0;
+        int high=n-1;
         int ans[] = {-1,-1};
         Arrays.sort(arr);
         
