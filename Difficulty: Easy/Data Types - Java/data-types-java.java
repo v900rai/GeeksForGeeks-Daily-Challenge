271ee390d888e5ee8d54edbc183031173544f207
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-29 12:39:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Data Types - Java/data-types-java.java
+++ Difficulty: Easy/Data Types - Java/data-types-java.java
@@ -1,13 +1,3 @@
-//{ Driver Code Starts
-//Initial Template for Java
-
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.util.*;
-
-
-// } Driver Code Ends
 
 //User function Template for Java
 
@@ -27,41 +17,3 @@ class Geeks{
         
     }
 }
-
-//{ Driver Code Starts.
-
-class GFG {
-    // Driver code
-	public static void main (String[] args) {
-	    
-	    // Input using Scanner class
-	    Scanner sc = new Scanner(System.in);
-		int a;
-		float b;
-		double c;
-		long l;
-		byte d;
-		
-		//Taking input number of testcases 
-		int t = sc.nextInt();
-		
-		while(t-- > 0){
-		    
-		    //taking input all the different datatypes in their respective variables
-		    a = sc.nextInt();
-		    b = sc.nextFloat();
-		    c = sc.nextDouble();
-		    l = sc.nextLong();
-		    d = sc.nextByte();
-		    
-		    //Creating an object of class Geeks
-		    Geeks g = new Geeks();
-		    
-		    //Calling dataTypes method of class Geeks
-		    g.dataTypes(a, b, c, l, d);
-		
-System.out.println("~");
-}
-	}
-}
-// } Driver Code Ends
