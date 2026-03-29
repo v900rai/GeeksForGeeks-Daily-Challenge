f59de61547afac177207a2677fd7a4b4b4d6d3d0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-03-29 12:35:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The New Line - Java/the-new-line-java.java
+++ Difficulty: Easy/The New Line - Java/the-new-line-java.java
@@ -3,12 +3,8 @@ class Geeks {
     // Function to print each word in single line
     static void printIndividualLine() {
 
-       // Your code here
-        String s = "Geeks for Geeks";
-        String[] str = s.split(" ");
-        for(int i = 0; i < str.length; i++){
-            System.out.println(str[i]);
-        }
-        
+        // Your code here
+                System.out.println("Geeks\nfor\nGeeks");
+
     }
 }
