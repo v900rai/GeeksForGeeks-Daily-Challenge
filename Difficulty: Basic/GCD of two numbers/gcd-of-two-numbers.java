4a745076217b25aef1a8a7c183a8ddd521e6b21a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-04-18 14:53:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.java
@@ -0,0 +1,14 @@
+
+
+
+class Solution {
+    public static int gcd(int a, int b) {
+        if(b==0){
+            return a;
+        }
+        else{
+            return gcd(b, a%b);
+        }
+    }
+}
+        
