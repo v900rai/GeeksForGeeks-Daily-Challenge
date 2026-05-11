1342f7fb9be20b892b2a657127e623993da3ab71
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-11 09:28:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Palindrome Pairs/palindrome-pairs.java
+++ Difficulty: Hard/Palindrome Pairs/palindrome-pairs.java
@@ -1,34 +1,64 @@
-// User function Template for Java
+import java.util.*;
 
 class Solution {
-    static boolean palindromepair(int N, String arr[]) {
-        for(int i=0;i<N-1;i++){
-            for(int j=i+1;j<N;j++){
-                
-            
-            if(arr[i].charAt(0)==arr[j].charAt(arr[j].length()-1) || arr[j].charAt(0)==arr[i].charAt(arr[i].length()-1))   
-                
-                  if(helper(arr[i]+arr[j]) || helper(arr[j]+arr[i]))
-                          return true;
+
+    static boolean palindromePair(String[] arr) {
+
+        int n = arr.length;
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            map.put(arr[i], i);
+        }
+
+        for(int i = 0; i < n; i++) {
+
+            String word = arr[i];
+            int len = word.length();
+
+            for(int j = 0; j <= len; j++) {
+
+                String left = word.substring(0, j);
+                String right = word.substring(j);
+
+                // Case 1
+                if(isPalindrome(left)) {
+
+                    String rev = new StringBuilder(right).reverse().toString();
+
+                    if(map.containsKey(rev) && map.get(rev) != i)
+                        return true;
+                }
+
+                // Case 2
+                if(j != len && isPalindrome(right)) {
+
+                    String rev = new StringBuilder(left).reverse().toString();
+
+                    if(map.containsKey(rev) && map.get(rev) != i)
+                        return true;
+                }
             }
         }
+
         return false;
     }
-      
-      static boolean helper(String s){
-          
-          int i=0;
-          int j=s.length()-1;
-          while(i<=j)
-          {
-              if(s.charAt(i)!=s.charAt(j))
-                   return false;
-                   
-                   i++;
-                   j--;
-          }
-          
-          return true;
-      }
-}
 
+    static boolean isPalindrome(String s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i < j) {
+
+            if(s.charAt(i) != s.charAt(j))
+                return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
