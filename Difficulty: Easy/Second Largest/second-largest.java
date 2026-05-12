dc6ffc125d803201812ff71bdab8fb58542a3da2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-12 21:18:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,22 +1,22 @@
 class Solution {
-    public int getSecondLargest(int[] arr) {
-        
-        int firstMax = Integer.MIN_VALUE;
-        int secondMax = Integer.MIN_VALUE;
-        
-        for(int num : arr)
-        {
-            if(num>firstMax)
-            {
-                secondMax= firstMax;
-                firstMax=num;
-                continue;
-            }
-            if(num>secondMax && num!=firstMax)
-            {
-                secondMax=num;
-            }
-        }
-        return secondMax == Integer.MIN_VALUE ? -1 :secondMax;
-    }
+	public int getSecondLargest(int[] arr) {
+		
+		int firstMax = Integer.MIN_VALUE;
+		int secondMax = Integer.MIN_VALUE;
+		
+		for (int num : arr)
+			{
+			if (num>firstMax)
+				{
+				secondMax = firstMax;
+				firstMax = num;
+				continue;
+			}
+			if (num>secondMax && num != firstMax)
+				{
+				secondMax = num;
+			}
+		}
+		return secondMax == Integer.MIN_VALUE ? -1 :secondMax;
+	}
 }
