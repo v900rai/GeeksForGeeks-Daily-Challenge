5496c199685b2be9bbb8dbff7e4705b7da54d01d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-24 21:52:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -1,15 +1,18 @@
 class Solution {
     public static void fizzBuzz(int number) {
         // Write your code here.
-        if(number%3 == 0 && number%5 != 0){
+        if(number % 3 == 0 && number %5 !=0){
             System.out.println("Fizz");
         }
+        
         else if(number%5 == 0 && number%3 != 0){
             System.out.println("Buzz");
         }
+        
         else if(number%15 == 0){
             System.out.println("FizzBuzz");
         }
+        
         else{
             System.out.println(number);
         }
