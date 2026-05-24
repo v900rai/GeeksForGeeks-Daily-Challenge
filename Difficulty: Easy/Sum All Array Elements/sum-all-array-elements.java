1d292107b9fb4ef387f5162d1ea442a68a9a1806
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-24 21:55:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
+++ Difficulty: Easy/Sum All Array Elements/sum-all-array-elements.java
@@ -1,12 +1,10 @@
-// User function Template for Java
-
 class Solution {
-    int arraySum(int arr[]) {
-        int sum =0;
-        for(int i=0;i<arr.length; i++){
-            
-            sum = sum + arr[i];
+    public int arraySum(List<Integer> arr) {
+        
+        int sum = 0;
+        for(int i=0; i<arr.size(); i++){
+            sum = sum + arr.get(i);
         }
         return sum;
     }
-}
+};
