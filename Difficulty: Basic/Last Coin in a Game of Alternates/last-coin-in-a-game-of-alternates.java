9a16792d29d2d7de4dc355f4f84c04fce5380398
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-24 09:54:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last Coin in a Game of Alternates/last-coin-in-a-game-of-alternates.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int coin(int[] arr) {
+        
+        int i = 0;
+        int j= arr.length-1;
+        while(i< j){
+            if(arr[i] > arr[j]){
+              i++;
+            }
+            else{
+                j--;
+            }
+        }
+        return arr[i];
+    }
+}
