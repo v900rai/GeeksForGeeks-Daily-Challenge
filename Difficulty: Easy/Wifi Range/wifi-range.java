5fac5e9a6659d346f9c4ac81c7e33890e274f521
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-05-27 13:22:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Wifi Range/wifi-range.java
+++ Difficulty: Easy/Wifi Range/wifi-range.java
@@ -1,27 +1,39 @@
-// User function Template for Java
 class Solution {
-    boolean wifiRange(int N, String S, int X) {
-        // code here
+    public boolean wifiRange(String s, int x) {
         
-       boolean []seen = new boolean[N];
-       Arrays.fill(seen,false);
-       for(int i=0;i<N;i++)
-       {
-           if(S.charAt(i) == '1')
-           {
-               for(int j = Math.max(0 , i - X); j <= Math.min(i + X , N - 1) ; j++)
-               {
-                   seen[j] = true;
-               }
-           }
-       }
-       for(int i=0;i<N;i++)
-       {
-           if(!seen[i])
-           {
-               return false;
-           }
-       }
-       return true;
+        int l = Integer.MAX_VALUE;
+        int r = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if (ch == '1') {
+                int lx = i - x;
+                if (lx < 0) lx = 0;
+                
+                int ry = i + x;
+                if (ry >= s.length()) ry = s.length()-1;
+                
+                // covers all rooms
+                if (lx == 0 && ry == s.length()-1) return true;
+                
+                // first interval
+                if (l == Integer.MAX_VALUE && r == Integer.MIN_VALUE) {
+                    l = lx;
+                    r = ry;
+                } else {
+                    // merge and check
+                    if (lx > r && lx - r > 1) {
+                        // gap found
+                        return false;
+                    }
+                    // merge interval
+                    l = Math.min(l, lx);
+                    r = Math.max(r, ry);
+                }
+            }
+        }
+        
+        return l == 0 && r == s.length()-1;
     }
 }
