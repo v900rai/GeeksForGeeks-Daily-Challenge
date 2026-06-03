d7c922df21882fcafd13fe037326741e717640f9
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-03 19:44:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.java
@@ -5,7 +5,8 @@ class Solution {
             System.out.println("Fizz");
         }
         
-        else if(number%5 == 0 && number%3 != 0){
+       
+        else if(number % 5 == 0 && number % 3 !=0){
             System.out.println("Buzz");
         }
         
