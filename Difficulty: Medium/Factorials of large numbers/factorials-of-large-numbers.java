cfb6a1b5e8e2d84ed471dea94275ef4f469589ef
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-09 23:02:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Factorials of large numbers/factorials-of-large-numbers.java
+++ Difficulty: Medium/Factorials of large numbers/factorials-of-large-numbers.java
@@ -1,24 +1,26 @@
-// User function Template for Java
+import java.util.*;
+import java.math.BigInteger;
 
 class Solution {
     public static ArrayList<Integer> factorial(int n) {
-        // code here
         ArrayList<Integer> res = new ArrayList<>();
-        res.add(1);
-        while(n>1){
-            int carry = 0;
-            for(int i=res.size()-1;i>=0;i--){
-                int mult = res.get(i)*n + carry;
-                carry = mult/10;
-                res.set(i,mult%10);
-            }
-            while(carry>0){
-                res.add(0,carry%10);
-                carry = carry/10;
-            }
-            n--;
+
+        // Step 1: Calculate factorial using BigInteger
+        BigInteger fact = BigInteger.ONE;
+        for (int i = 1; i <= n; i++) {
+            fact = fact.multiply(BigInteger.valueOf(i));
         }
+
+        // Step 2: Extract digits
+        while (fact.compareTo(BigInteger.ZERO) > 0) {
+            int rem = fact.mod(BigInteger.TEN).intValue();
+            res.add(rem);
+            fact = fact.divide(BigInteger.TEN);
+        }
+
+        // Step 3: Reverse to correct order
+        Collections.reverse(res);
+
         return res;
     }
 }
-
