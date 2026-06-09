75d65f831049bac97c32c73c1fb5a55fe71c8c40
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-09 22:59:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
+++ Difficulty: Medium/Strings Rotations of Each Other/strings-rotations-of-each-other.java
@@ -1,16 +1,55 @@
 class Solution {
     public boolean areRotations(String s1, String s2) {
-        // code here
-        s1 = s1 + s1;
-        int i = 0;
-        int j = 0;
-        while(i < s1.length() && j < s2.length()){
-            if(s1.charAt(i) == s2.charAt(j)){
+        if (s1.length() != s2.length()) return false;
+        
+        String txt = s1 + s1;
+        String pat = s2;
+        
+        return kmpSearch(txt, pat);
+    }
+    private boolean kmpSearch(String txt, String pat) {
+        int n = txt.length();
+        int m = pat.length();
+        int[] lps = computeLPSArray(pat, m);
+        
+        int i = 0; // index for txt
+        int j = 0; // index for pat
+        while (i < n) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
                 j++;
             }
-            i++;
+            if (j == m) {
+                return true; // Pattern found
+            } else if (i < n && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i++;
+                }
+            }
         }
-        if(j == s2.length()) return true;
         return false;
+    }
+
+    private int[] computeLPSArray(String pat, int m) {
+        int[] lps = new int[m];
+        int len = 0;
+        int i = 1;
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
     }
 }
