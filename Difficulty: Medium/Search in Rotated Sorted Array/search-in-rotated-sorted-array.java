9876ba51fd3cf42288ee5e8a306e9889d5bd8d1a
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-13 13:07:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.java
@@ -1,14 +1,12 @@
 
-
 // User function Template for Java
-
 class Solution {
-    int search(int[] arr, int key) {
-       for(int i=0; i<arr.length; i++){
-           if(arr[i]==key){
-               return i;
-           }
-       }
-       return -1;
+    int search(int[] arr, int key){
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == key){
+                return i;
+            }
+        }
+        return -1;
     }
 }
