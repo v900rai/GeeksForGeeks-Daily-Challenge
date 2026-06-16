5a76b743e3374c08d93bd26810412c98e1571760
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-16 22:50:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
@@ -1,46 +1,42 @@
+/* Structure of linked list Node
+class Node {
+    int data;
+    Node next;
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+} */
 class Solution {
     public static void removeLoop(Node head) {
-
-        if (head == null || head.next == null)
-            return;
+        if(head == null || head.next == null) return;
 
         Node slow = head;
         Node fast = head;
 
-        // Step 1: Detect loop
-        boolean hasLoop = false;
-
-        while (fast != null && fast.next != null) {
+       
+        while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
 
-            if (slow == fast) {
-                hasLoop = true;
-                break;
+            if(slow == fast){
+
+                
+                slow = head;
+                if(slow == fast){
+                    while(fast.next != slow){
+                        fast = fast.next;
+                    }
+                }
+                else{
+                    while(slow.next != fast.next){
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                }
+                fast.next = null;
+                return;
             }
         }
-
-        // Agar loop hi nahi hai
-        if (!hasLoop)
-            return;
-
-        // Step 2: Find starting point of loop
-        slow = head;
-
-        // Special case: loop starts at head
-        if (slow == fast) {
-            while (fast.next != slow) {
-                fast = fast.next;
-            }
-        } else {
-            // Normal case
-            while (slow.next != fast.next) {
-                slow = slow.next;
-                fast = fast.next;
-            }
-        }
-
-        // Step 3: Remove loop
-        fast.next = null;
     }
 }
