4141bb8c1460534eff4e3a25af66d11598a5a3ad
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-06-24 16:48:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Closest Number/closest-number.java
+++ Difficulty: Basic/Closest Number/closest-number.java
@@ -1,34 +1,34 @@
 class Solution {
-    static int closestNumber(int n, int m) {
-        // code here
-        int ans = Integer.MIN_VALUE;
-        int countf = 0;
-        int temp = n;
-        int counts = 0;
-        while(true) {
-            counts++;
-            if(temp %m == 0) {
-                if(Math.abs(ans) < Math.abs(temp)) {
-                    ans = temp;
-                }
-                break;
-            }
-            temp++;
-        }
-        temp = n;
-        while(true) {
-            countf++;
-            if(temp %m ==0) {
-                if(countf < counts) {
-                    ans = temp;
-                } else if(countf == counts && Math.abs(ans) < Math.abs(temp)) {
-                    ans = temp;
-                }
-                break;
-            }
-            temp--;
-        }
-
-        return ans;
-    }
+	static int closestNumber(int n, int m) {
+		// code here
+		int ans = Integer.MIN_VALUE;
+		int countf = 0;
+		int temp = n;
+		int counts = 0;
+		while (true) {
+			counts++;
+			if (temp %m == 0) {
+				if (Math.abs(ans) < Math.abs(temp)) {
+					ans = temp;
+				}
+				break;
+			}
+			temp++;
+		}
+		temp = n;
+		while (true) {
+			countf++;
+			if (temp %m == 0) {
+				if (countf < counts) {
+					ans = temp;
+				} else if (countf == counts && Math.abs(ans) < Math.abs(temp)) {
+					ans = temp;
+				}
+				break;
+			}
+			temp--;
+		}
+		
+		return ans;
+	}
 }
