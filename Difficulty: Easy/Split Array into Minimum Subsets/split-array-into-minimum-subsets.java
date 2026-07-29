a4e79025c0e9d52f962fa332d4c48ff5aec7feff
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-07-29 12:34:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Array into Minimum Subsets/split-array-into-minimum-subsets.java
@@ -0,0 +1,13 @@
+class Solution {
+    int minSubsets(int arr[]) {
+       
+        Arrays.sort(arr);
+        int count = 1;
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] != arr[i-1] + 1){
+                count++;
+            }
+        }
+        return count;
+    }
+}
