194bbb978fbd145e3900f520ef5b7838c71b2db2
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-09 17:06:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.java
+++ Difficulty: Basic/Factorial/factorial.java
@@ -1,14 +1,12 @@
-
-
 class Solution {
-    static int factorial(int n) {
-        if(n==0 && n==1){
-            return 1;
-        }
-        int fact=1;
-        for(int i=2; i<=n; i++){
-            fact=fact*i;
-        }
-        return fact;
-    }
+	static int factorial(int n) {
+		if (n == 0 && n == 1) {
+			return 1;
+		}
+		int fact = 1;
+		for (int i = 2; i <= n; i++) {
+			fact = fact*i;
+		}
+		return fact;
+	}
 }
