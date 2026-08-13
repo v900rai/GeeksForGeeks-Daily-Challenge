b131514fda30077257e1618f0478abb032f2533e
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-13 13:45:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Express as Consecutive Number Sum/express-as-consecutive-number-sum.java
+++ Difficulty: Easy/Express as Consecutive Number Sum/express-as-consecutive-number-sum.java
@@ -1,7 +1,13 @@
 class Solution {
     public boolean isSumOfConsecutive(int n) {
-        return (n & (n - 1)) != 0;
-        
-        
+        // code here
+        if(n==1) return false;
+        while(n > 1){
+            if(n%2 !=0){
+                return true;
+            }
+            n=n/2;
+        }
+        return false;
     }
 }
