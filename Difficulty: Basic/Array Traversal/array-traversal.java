260568662408e3251c6c11ac154717134266ac3d
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-16 23:04:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Traversal/array-traversal.java
+++ Difficulty: Basic/Array Traversal/array-traversal.java
@@ -1,8 +1,9 @@
+
 class Solution {
     public static void arrayTraversal(int[] arr) {
         // Code here
-        for(int n: arr){
-            System.out.print(n+" ");
+        for(int  i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
         }
     }
 }
