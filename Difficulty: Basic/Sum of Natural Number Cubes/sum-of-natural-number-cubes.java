cf5dc62569ee96b2b2ac8f0c366ce16972d6b8d0
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-21 16:27:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Natural Number Cubes/sum-of-natural-number-cubes.java
+++ Difficulty: Basic/Sum of Natural Number Cubes/sum-of-natural-number-cubes.java
@@ -1,11 +1,11 @@
 class Solution {
-     int sumOfSeries(int n) {
-         int sum = 0;
-
-         for(int i = 1; i <= n; i++) {
-             sum = sum + i * i * i;
-         }
-
-         return sum;
-     }
- }
+	int sumOfSeries(int n) {
+		int sum = 0;
+		
+		for (int i = 1; i <= n; i++) {
+			sum = sum + i *i *i;
+		}
+		
+		return sum;
+	}
+}
