fd5ea9e2eb7d1929a280543f74e0f94c46cba544
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-21 16:51:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Divisible by 13/divisible-by-13.java
+++ Difficulty: Medium/Divisible by 13/divisible-by-13.java
@@ -1,27 +1,28 @@
 class Solution {
-    public boolean divby13(String s) {
-        // code here
-        int n = s.length();
-        if(n==0)return true;
-        StringBuilder st = new StringBuilder();
-        for(int i = 0;i<n;i++)
-        {
-            st.append(s.charAt(i));
-            while(i+1<n&&Integer.parseInt(st.toString())<13)
-            {
-                i++;
-                st.append(s.charAt(i));
-            }
-            int num = Integer.parseInt(st.toString());
-            int k = num%13;
-            if(k!=0)
-            {
-                st = new StringBuilder(String.valueOf(k));
-            }
-            else{
-                st = new StringBuilder();
-            }
-        }
-        return st.length()==0;
-    }
+	public boolean divby13(String s) {
+		// code here
+		int n = s.length();
+		if (n == 0)
+		return true;
+		StringBuilder st = new StringBuilder();
+		for (int i = 0; i<n; i++)
+			{
+			st.append(s.charAt(i));
+			while (i + 1<n && Integer.parseInt(st.toString())<13)
+				{
+				i++;
+				st.append(s.charAt(i));
+			}
+			int num = Integer.parseInt(st.toString());
+			int k = num%13;
+			if (k != 0)
+				{
+				st = new StringBuilder(String.valueOf(k));
+			}
+			else {
+				st = new StringBuilder();
+			}
+		}
+		return st.length() == 0;
+	}
 }
