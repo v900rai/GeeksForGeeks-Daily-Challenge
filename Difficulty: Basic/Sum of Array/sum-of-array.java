336dc7d8147d97138042c01b5d79b75570d8726c
Vishalrai@24 <97247801+v900rai@users.noreply.github.com>
2026-08-30 14:57:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Array/sum-of-array.java
+++ Difficulty: Basic/Sum of Array/sum-of-array.java
@@ -1,11 +1,12 @@
-// User function Template for Java
-
 class Solution {
-    int arraySum(int arr[]) {
+    public int arraySum(int arr[]) {
+        // code here
+        
         int sum =0;
-        for(int i=0;i<arr.length; i++){
-            sum = sum + arr[i];
+        for(int i=0; i<arr.length; i++){
+            sum = sum +arr[i];
         }
         return sum;
+        
     }
 }
